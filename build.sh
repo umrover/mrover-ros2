f9be5afa5d8f4f84432fd5502e367538672e360b
Ali Ryckman <alison.ryckman@gmail.com>
2024-08-20 17:58:08 -0400

    add sim python launch files

--- build.sh
+++ build.sh
@@ -8,8 +8,10 @@ pushd ../..
 export CC=clang-18
 export CXX=clang++-18
 
+
+# TODO (ali): add build configs for debug vs release
 colcon build \
-	--cmake-args -G Ninja -W no-dev \
+	--cmake-args -G Ninja -W no-dev -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 	--symlink-install \
 	--event-handlers console_direct+ \
 	$@
