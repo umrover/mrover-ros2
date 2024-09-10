e79966762df4a504b0d3e3a29b7b1f9ca76cf03a
Ali Ryckman <alison.ryckman@gmail.com>
2024-09-09 20:32:41 -0400

    configure clang symlinks

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ set -euxo pipefail
 # Build in the colcon workspace, not the package
 pushd ../..
 
+# set C/CXX compilers
+export CC=clang
+export CXX=clang++ 
+
 # TODO (ali): add build configs for debug vs release
 colcon build \
 	--cmake-args -G Ninja -W no-dev -DCMAKE_BUILD_TYPE=RelWithDebInfo \
