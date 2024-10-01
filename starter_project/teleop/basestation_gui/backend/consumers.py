04844d2558debf2a946a4380b17b1df2530e8f94
TJ Neuenfeldt <tjneuenfeldt@gmail.com>
2024-10-01 09:30:44 -0400

    node in separate file

--- starter_project/teleop/basestation_gui/backend/consumers.py
+++ starter_project/teleop/basestation_gui/backend/consumers.py
@@ -16,27 +16,32 @@ from backend.input import DeviceInputs
 from mrover.msg import WheelCmd
 from geometry_msgs.msg import Twist
 
-node = rclpy.create_node('teleoperation')
+from backend.ros2_utils import get_ros2_node_instance
+
+# node = rclpy.create_node('teleoperation')
 
 class GUIConsumer(JsonWebsocketConsumer):
-    subscribers: list[Subscription] = []
-    node: Node = None
-
+    subscribers = []
+    
     def connect(self) -> None:
+        self.node = get_ros2_node_instance()
+        self.forward_ros_topic("/wheel_cmd", WheelCmd, "wheel_cmd")
         self.accept()
-
-        # node = rclpy.create_node('teleoperation')
 
         ########################################################################################
         # Use self.forward_ros_topic when you want to get data from a ROS topic to a GUI
         # without needing any modifications done on it. For instance, reading motor output.
         ########################################################################################
-        self.forward_ros_topic("/wheel_cmd", WheelCmd, "wheel_cmd")
-        # rclpy.spin(node)
 
-    def disconnect(self, close_code) -> None:
-        for subscriber in self.subscribers:
-            node.destroy_subscription(subscriber)
+
+    # def ros2_spin(self):
+    #     while rclpy.ok():
+    #         rclpy.spin_once(self.node)
+
+    # def disconnect(self, close_code) -> None:
+    #     rclpy.shutdown()
+    #     # for subscriber in self.subscribers:
+    #     #     node.destroy_subscription(subscriber)
 
     def forward_ros_topic(self, topic_name: str, topic_type: Type, gui_msg_type: str) -> None:
         """
@@ -50,17 +55,17 @@ class GUIConsumer(JsonWebsocketConsumer):
         def callback(ros_message: Any):
             # Formatting a ROS message as a string outputs YAML
             # Parse it back into a dictionary, so we can send it as JSON
-            node.get_logger().error(f"{ros_message}")
+            self.node.get_logger().error(f"{ros_message}")
             self.send_message_as_json({"type": gui_msg_type})
             # self.send_message_as_json({"type": gui_msg_type, **yaml.safe_load(str(ros_message))})
-        self.subscribers.append(node.create_subscription(topic_type, topic_name , callback, qos_profile=1))
-        node.get_logger().error(f"{self.subscribers[0].callback}")
+        self.subscribers.append(self.node.create_subscription(topic_type, topic_name , callback, qos_profile=1))
+        self.node.get_logger().error(f"{self.subscribers[0]}")
 
     def send_message_as_json(self, msg: dict):
         try:
             self.send(text_data=json.dumps(msg))
         except Exception as e:
-            node.get_logger().warning(f"Failed to send message: {e}")
+            self.node.get_logger().warning(f"Failed to send message: {e}")
 
 
     def receive(self, text_data=None, bytes_data=None, **kwargs) -> None:
@@ -71,13 +76,13 @@ class GUIConsumer(JsonWebsocketConsumer):
         """
 
         if text_data is None:
-            node.get_logger().warning("Expecting text but received binary on GUI websocket...")
+            # node.get_logger().warning("Expecting text but received binary on GUI websocket...")
             return
 
         try:
             message = json.loads(text_data)
         except json.JSONDecodeError as e:
-            node.get_logger().warning(f"Failed to decode JSON: {e}")
+            # node.get_logger().warning(f"Failed to decode JSON: {e}")
             return
 
         try:
@@ -90,8 +95,10 @@ class GUIConsumer(JsonWebsocketConsumer):
                     device_input = DeviceInputs(axes, buttons)
                     send_joystick_twist(device_input)
                 case _:
-                    node.get_logger().warning(f"Unhandled message: {message}")
+                    # node.get_logger().warning(f"Unhandled message: {message}")
+                    pass
 
         except:
-            node.get_logger().error(f"Failed to handle message: {message}")
-            node.get_logger().error(traceback.format_exc())
+            # node.get_logger().error(f"Failed to handle message: {message}")
+            # node.get_logger().error(traceback.format_exc())
+            pass
