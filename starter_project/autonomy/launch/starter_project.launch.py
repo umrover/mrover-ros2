90a6a767b4459ca01ca4cbc0531cc55c807c1520
jbrhm <jrabraham1904@gmail.com>
2024-10-17 20:03:43 -0400

    Runs

--- starter_project/autonomy/launch/starter_project.launch.py
+++ starter_project/autonomy/launch/starter_project.launch.py
@@ -58,10 +58,10 @@ def generate_launch_description():
         # ===========
         # Navigation
         # ===========
-        # navigation_node,
+        navigation_node,
 
         # ============
         # Localization
         # ============
-        # localization_node
+        localization_node
     ])
