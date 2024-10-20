cc64931cef262b3abece81d68b9c890244bfc180
jaskifstad <juliansk@umich.edu>
2024-10-20 14:06:19 -0400

    initial test launch files

---
+++ launch/basestation.launch.py
@@ -0,0 +1,28 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from launch.conditions import LaunchConfigurationEquals
+
+
+def generate_launch_description():
+
+
+    basestation_gps_driver_node = Node(
+        package="mrover",
+        executable="basestation_gps_driver.py",
+        name="basestation_gps_driver",
+        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml")],
+    )
+
+    
+    return LaunchDescription(
+        [
+            basestation_gps_driver_node,
+        ]
+    )
