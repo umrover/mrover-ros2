878dbf5e63adf1e7283da86e0da73a56bd671d40
Jonah Kelman <jonahkel@umich.edu>
2024-10-27 14:26:48 -0400

    Made testable code for ik testing

--- scripts/ik_testing.py
+++ scripts/ik_testing.py
@@ -1,20 +1,26 @@
 
 from rclpy.node import Node
+import rclpy
 
 from mrover.srv import IkTest
 
 
 
-class IK_Testing:
-    node: Node
+class IK_Testing(Node):
 
     def __init__(self, node:Node):
-        self.node = node
+        super().__init__("ik_testing")
 
     def test_points(self):
+        ROWS = 1
+        COLS = 1
+        pointMap = [[False] * ROWS for i in range(ROWS)]
+
+        row = 0
+        col = 0
         request = IkTest.Request()
         request._target._header._stamp = self.node.get_clock().now()
-        request.target.header._frame_id = "test_point"
+        request.target.header._frame_id = "arm_base_link"
         request.target.pose._position.x = request.target.pose._position.y = request.target.pose._position.z = 0
 
 
@@ -23,5 +29,16 @@ class IK_Testing:
             raise RuntimeError("IK testing not working :(")
 
         future = client.call_async(request)
-        future.result.
+        rclpy.spin_until_future_complete(self, future)        
+        pointMap[row][col] = future.result().success
+        return pointMap
 
+
+def main(args = None):
+    rclpy.init(args)
+
+
+    ik_tester = IK_Testing()
+    ik_tester.get_logger().info(ik_tester.test_points())
+
+    rclpy.shutdown()
