649c7b9b912dafa8dbec293301fdeb317d008d96
ejhong <ejhong@umich.edu>
2024-11-08 04:22:30 -0500

    forgot to include the added threshold parameter for last commit

--- navigation/nav.py
+++ navigation/nav.py
@@ -67,6 +67,7 @@ class Navigation(Node):
                 ("search.traversable_cost", Parameter.Type.DOUBLE),
                 ("search.update_delay", Parameter.Type.DOUBLE),
                 ("search.safe_approach_distance", Parameter.Type.DOUBLE),
+                ("search.a_star_thresh", Parameter.Type.DOUBLE),
 
                 ("single_tag.stop_threshold", Parameter.Type.DOUBLE),
                 ("single_tag.tag_stop_threshold", Parameter.Type.DOUBLE),
