f7cd747343289b1c0885c22bbbcba0ee42bf7c1f
jbrhm <jrabraham1904@gmail.com>
2024-11-17 14:15:33 -0500

    working tf

--- scripts/move_cost_map.py
+++ scripts/move_cost_map.py
@@ -10,6 +10,7 @@ import tf2_ros
 from rclpy.node import Node
 from mrover.srv import MoveCostMap
 from lie.conversions import SE3, from_position_orientation, from_tf_tree
+from lie.lie import SE3Conversions
 
 class MoveCostMapNode(Node):
     def __init__(self) -> None:
@@ -17,20 +18,21 @@ class MoveCostMapNode(Node):
 
         self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
         self.tf_buffer = tf2_ros.Buffer()
-        tf2_ros.TransformListener(self.tf_buffer, self)
-        position = from_tf_tree(self.tf_buffer, 'base_link', 'map')
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
+        self.create_timer(1, self.move_request)
+
+    def move_request(self):
         self.move_cost_map_srv = self.create_client(MoveCostMap, 'move_cost_map')
 
         while not self.move_cost_map_srv.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
 
-        tx, ty, tz = position.translation()
+        position = SE3.from_tf_tree(self.tf_buffer, 'base_link', 'map')
+        tx, ty, _ = position.translation()
         course_in_map = from_position_orientation(tx, ty)
 
         SE3.to_tf_tree(self.tf_broadcaster, course_in_map, "debug_course", "map")
-
-        time.sleep(0.01)
 
         self.req = MoveCostMap.Request()
 
@@ -43,14 +45,10 @@ class MoveCostMapNode(Node):
 
 
 def main() -> None:
-    try:
-        rclpy.init(args=sys.argv)
-        rclpy.spin(MoveCostMapNode())
-        rclpy.shutdown()
-    except KeyboardInterrupt:
-        pass
-    except ExternalShutdownException:
-        pass
+    rclpy.init(args=sys.argv)
+    node = MoveCostMapNode()
+    rclpy.spin(node=node)
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
