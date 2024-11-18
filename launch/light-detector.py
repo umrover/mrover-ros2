cb3c6cf632be997f2e021245deffba3f0086064b
MRover Perception <umroverperception@gmail.com>
2024-11-18 18:32:16 -0500

    Okay

--- launch/light-detector.py
+++ launch/light-detector.py
@@ -11,29 +11,14 @@ from launch.conditions import LaunchConfigurationEquals
 
 
 def generate_launch_description():
-    zed_node = Node(
-        package="mrover",
-        executable="zed",
-        name="zed_wrapper",
-        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "zed.yaml")],
-    )
-
-    base_link_to_zed = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        arguments=["0", "0", "1", "0", "0", "0", "1", "base_link", "zed_left_camera_frame"],
-    )
-
     light_detector_position_node = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
-        arguments=["0", "0", "0", "0", "0", "0", "mCameraFrame", "mWorldFrame", "100"],
+        arguments=["0", "0", "0", "0", "0", "0", "zed_left_camera_frame", "mWorldFrame"],
     )
 
     return LaunchDescription(
         [
-            zed_node,
-            base_link_to_zed,
             light_detector_position_node,
         ]
     )
