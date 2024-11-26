fd1be80a84e1d20428c9480ba7afed604ba1ee7b
jbrhm <jrabraham1904@gmail.com>
2024-11-26 10:34:42 -0500

    fixed formatting

--- tensorrt/inference_wrapper.cpp
+++ tensorrt/inference_wrapper.cpp
@@ -5,24 +5,24 @@
 using namespace nvinfer1;
 
 /**
-* cudaMemcpys CPU memory in inputTensor to GPU based on bindings
-* Queues that tensor to be passed through model
-* cudaMemcpys the result back to CPU memory
-* Requires bindings, inputTensor, stream
-* Modifies stream, outputTensor
-*/
+ * cudaMemcpys CPU memory in inputTensor to GPU based on bindings
+ * Queues that tensor to be passed through model
+ * cudaMemcpys the result back to CPU memory
+ * Requires bindings, inputTensor, stream
+ * Modifies stream, outputTensor
+ */
 
 InferenceWrapper::InferenceWrapper(std::string const& modelName, std::string const& packagePath) {
-    mInference = std::make_shared<Inference>(modelName, packagePath);
+	mInference = std::make_shared<Inference>(modelName, packagePath);
 }
 
 auto InferenceWrapper::doDetections(cv::Mat const& img) const -> void {
-    // Execute the forward pass on the inference object
-    mInference->doDetections(img);
+	// Execute the forward pass on the inference object
+	mInference->doDetections(img);
 }
 
 auto InferenceWrapper::getOutputTensor() const -> cv::Mat {
-    return mInference->getOutputTensor();
+	return mInference->getOutputTensor();
 }
 
 auto InferenceWrapper::getInputTensorSize() -> std::vector<int64_t>{
