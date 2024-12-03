17140af629c98951af9076aceef1a8a115984e42
Pearl Lin <pearllin@umich.edu>
2024-12-02 19:11:50 -0500

    yet more iekf

--- localization/iekf/InvariantEKF.m
+++ localization/iekf/InvariantEKF.m
@@ -4,10 +4,22 @@ classdef InvariantEKF < handle
     properties
         X
         P
-        Q
-        % TODO: confirm A is correct
+
+        % TODO: confirm A is correct, and A should be static
         A
        
+    end
+
+    methods(Static)
+
+        % put vector into Lie algebra basis
+        function xhat = wedge(x)
+            xhat = [0 -x(3) x(2) x(4) x(7);
+                    x(3) 0 -x(1) x(5) x(8);
+                    -x(2) x(1) 0 x(6) x(9);
+                    0 0 0 0 0;
+                    0 0 0 0 0];
+        end
     end
 
     methods
@@ -25,7 +37,6 @@ classdef InvariantEKF < handle
 
             obj.P = zeros(9);
 
-            obj.Q = eye(9);
         end
 
         % adjoint
@@ -37,64 +48,64 @@ classdef InvariantEKF < handle
                     v_skew * obj.X(1:3,1:3), obj.X(1:3,1:3), zeros(3);
                     p_skew * obj.X(1:3,1:3), zeros(3), obj.X(1:3,1:3)];
         end
-        
-        
-        % put vector into Lie algebra basis
-        function xhat = wedge(obj, x)
-            xhat = [0 -x(3) x(2) x(4) x(7);
-                    x(3) 0 -x(1) x(5) x(8);
-                    -x(2) x(1) 0 x(6) x(9);
-                    0 0 0 0 0;
-                    0 0 0 0 0];
-        end
 
         % w is 3x1 matrix of angular velocities (rad/s)
-        function gyro_predict(obj, w, dt)
+        % q is the 3x3 covariance matrix
+        function gyro_predict(obj, w, q, dt)
 
             w_skew = [0 -w(3) w(2); w(3) 0 -w(1); -w(2) w(1) 0];
             Ad_X = obj.adjoint_X();
-            
-            % TODO: does Q change??
+            Q = [q, zeros(3,3), zeros(3,3);
+                 zeros(3,3), zeros(3,3), zeros(3,3);
+                 zeros(3,3), zeros(3,3), zeros(3,3)];
+            Q_d = expm(obj.A * dt) * Q * dt * (expm(obj.A * dt));
+
 
             % propogate
             obj.X(1:3,1:3) = obj.X(1:3,1:3) * expm(w_skew * dt);
-            obj.P = expm(obj.A * dt) * obj.P * (expm(obj.A * dt))' + Ad_X * obj.Q * Ad_X'; % TODO: Q here is wrong, take another look at this
+            obj.P = expm(obj.A * dt) * obj.P * (expm(obj.A * dt))' + Ad_X * Q_d * Ad_X';
 
         end
 
         % a is 3x1 matrix of linear acceleration
-        function accel_predict(obj, a, dt)
-            
+        % q is the 3x3 covariance matrix
+        function accel_predict(obj, a, q, dt)
+
             g = [0; 0; 9.81];
             Ad_X = obj.adjoint_X();
+            Q = [zeros(3,3), q, zeros(3,3);
+                 zeros(3,3), zeros(3,3), zeros(3,3);
+                 zeros(3,3), zeros(3,3), zeros(3,3)];
+            Q_d = expm(obj.A * dt) * Q * dt * (expm(obj.A * dt)); % TODO: check during testing
 
             % propogate
             temp = obj.X;
             temp(1:3,4) = obj.X(1:3,4) + obj.X(1:3,1:3) * a * dt - g * dt; % velocity
             temp(1:3,5) = obj.X(1:3,5) + obj.X(1:3,4) * dt + 0.5 * obj.X(1:3,1:3) * a * dt.^2 - 0.5 * g * dt.^2; % position
-            obj.P = expm(obj.A * dt) * obj.P * (expm(obj.A * dt))' + Ad_X * obj.Q * Ad_X'; % TODO: Q here is wrong, take another look at this
+
             obj.X = temp;
+            obj.P = expm(obj.A * dt) * obj.P * (expm(obj.A * dt))' + Ad_X * Q_d * Ad_X';
 
         end
         
-        % p is a 3x1 matrix of cartesian position, n is 3x1 matrix of
-        % associated noise
-        function position_update(obj, p, n)
+        % p is a 3x1 matrix of cartesian position
+        % V is the 3x1 noise
+        function position_update(obj, p, V)
             H = [zeros(3), zeros(3), -eye(3)];
 
-            % get n, p into the robot frame
-            n = -obj.X(1:3,1:3)' * n;
+            % get V, p into the robot frame
+            V = -obj.X(1:3,1:3)' * V;
             p = -obj.X(1:3,1:3)' * p;
 
-            N = obj.X(1:3,1:3) * blkdiag(n(1), n(2), n(3)) / obj.X(1:3,1:3);
-            Y = [p; 0; 1];
+            Y = [p; 0; 1] + [V; 0; 0];
             b = [zeros(3,1); 0; 1];
             innov = obj.X * Y - b;
-
+            
+            N = obj.X(1:3,1:3) * blkdiag(V(1), V(2), V(3)) * obj.X(1:3,1:3)';
             S = H * obj.P * H' + N;
             L = obj.P * H' / S;
 
-            delta = obj.wedge(L * innov(1:3,1));
+            delta = InvariantEKF.wedge(L * innov(1:3,1));
             
             % correction
             obj.X = expm(delta) * obj.X;
@@ -102,12 +113,23 @@ classdef InvariantEKF < handle
             
         end
         
-        % TODO
 
         % m is mag heading measurement in radians
-        function mag_update(obj, m)
+        % V is the 10x1 noise in b
+        function mag_update(obj, m, V)
 
-            disp(obj.X);
+            g_skew = [0 -9.81 0;
+                      9.81 0 0;
+                      0 0 0];
+
+            m_skew = [0 1 -1;
+                      -1 0 1;
+                      1 -1 0];
+
+            H = [g_skew zeros(3,3) zeros(3,3);
+                 zeros(2,3) zeros(2,3) zeros(2,3);
+                 m_skew zeros(3,3) zeros(3,3);
+                 zeros(2,3) zeros(2,3) zeros(2,3)];
             
             % get mag measurement in the robot frame
             eulerXYZ = rotm2eul(obj.X(1:3,1:3));
@@ -121,52 +143,20 @@ classdef InvariantEKF < handle
 
             b = [0; 0; -9.81; 0; 0; 1; 1; 1; 0; 0];
             Y = [inv(X_m) zeros(5,5);
-                 zeros(5,5), inv(X_m)] * b;
-
-
-
-            % disp(obj.X);
-            % disp(X_m);
-
-            %disp(Y);
+                 zeros(5,5), inv(X_m)] * b + V;
 
             innov = [obj.X zeros(5,5);
                      zeros(5,5), obj.X] * Y - b;
-            % disp(innov);
 
-            g_skew = [0 -9.81 0;
-                      0 0 9.81;
-                      0 0 0];
-
-            m_skew = [1 1 0;
-                      -1 0 1;
-                      0 -1 -1];
-
-            H = [g_skew zeros(3,3) zeros(3,3);
-                 zeros(2,3) zeros(2,3) zeros(2,3);
-                 m_skew zeros(3,3) zeros(3,3);
-                 zeros(2,3) zeros(2,3) zeros(2,3)];
-
-            S = H * obj.P * H' + eye(10);
-            % disp(S);
-            % disp(obj.P);
-            % disp(H);
+            N = [obj.X zeros(5,5); zeros(5,5), obj.X] * blkdiag(V(1), V(2), V(3), V(4), V(5), V(6), V(7), V(8), V(9), V(10)) * [obj.X zeros(5,5); zeros(5,5), obj.X]';
+            S = H * obj.P * H' + N;
             L = obj.P * H' / S;
             
-            delta = obj.wedge(L * innov);
-            % disp(L * innov);
+            delta = InvariantEKF.wedge(L * innov);
             
+            % correction
             obj.X = expm(delta) * obj.X;
-            disp(obj.X);
-
-
-            % b_2 = [1; 1; 1];
-            % Y_2 = R_m' * b_2;
-            % innov_2 = obj.X(1:3,1:3) * Y_2 - b_2;
-            % disp(innov_2);
-
-
-
+            obj.P = (eye(9) - L * H) * obj.P * (eye(9) - L * H)' + L * N * L';
             
         end
 
