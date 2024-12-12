8fe5e8d4577d8b8a3e25ec12a682bb9060b0421f
TJ Neuenfeldt <tjneuenfeldt@gmail.com>
2024-12-12 12:01:30 -0500

    cam placement fixed, styling applied

--- teleoperation/basestation_gui/frontend/src/components/Cameras.vue
+++ teleoperation/basestation_gui/frontend/src/components/Cameras.vue
@@ -6,25 +6,25 @@
   </div>
 
   <div class="row">
-    <div v-for="(mission, index) in missionType" :key="index" class="col-sm feed">
+    <div v-for="(mission, index) in missionType" :key="index" class="col-sm">
       <div class="form-check d-flex justify-content-center align-items-center">
         <input
           v-model="selectedMission"
-          class="form-check-input"
+          class="btn-check"
           type="radio"
           :id="'mission' + index"
           :value="mission"
         />
-        <label class="form-check-label" :for="'mission' + index">{{ mission }}</label>
+        <label class="btn btn-outline-secondary" :for="'mission' + index">{{ mission }}</label>
       </div>
     </div>
   </div>
 
-  <div class="row">
+  <div class="row sticky-top bg-white">
       <div
         v-for="(camera, index) in cameras[selectedMission]"
         :key="camera"
-        class="col-sm feed"
+        class="col d-flex justify-content-center my-5"
       >
         <ToggleButton
           :id="camera"
@@ -34,25 +34,20 @@
           @change="toggleCamera(index)"
         />
       </div>
-    </div>
+  </div>
 
   <div class="container-fluid">
     <div class="row gx-3 gy-3 justify-content-center">
       <div
-        :class="['col-12', (numEnabled > 1) ? 'col-md-6' : '']"
-        v-for="(cam, index) in cameras[selectedMission]"
-        :key="cam"
+        :class="['col-12', (camsStreaming.length > 1) ? 'col-md-6' : '', 'camera-feed-container']"
+        v-for="index in camsStreaming"
+        :key="index"
       >
-        <div 
-          class="camera-feed-container "
-          v-if="camsEnabled[index]"
-        >
-          <CameraFeed
-            :mission="selectedMission"
-            :id="index"
-            :name="cam"
-          ></CameraFeed>
-        </div>
+        <CameraFeed
+          :mission="selectedMission"
+          :id="index"
+          :name="cameras[selectedMission][index]"
+        ></CameraFeed>
       </div>
     </div>
   </div> 
@@ -83,7 +78,7 @@ export default {
         "Autonomy Mission": ["Cam9" ,"Cam1"]
       },
       camsEnabled: [], // stores cam enabled state for cameras in selected mission (default true)
-      numEnabled: 0
+      camsStreaming: []
     }
   },
 
@@ -96,14 +91,8 @@ export default {
 
     selectedMission(newMission: string) {
       this.camsEnabled = new Array(this.cameras[newMission].length).fill(true);
+      this.camsStreaming = [...Array(this.camsEnabled.length).keys()];
     },
-
-    camsEnabled: {
-      handler: function() {
-      this.numEnabled = this.camsEnabled.filter((x: boolean) => x).length;
-      },
-      deep: true
-    }
   },
 
   computed: {
@@ -112,6 +101,7 @@ export default {
 
   created: function() {
     this.camsEnabled = new Array(this.cameras[this.selectedMission].length).fill(true);
+    this.camsStreaming = [...Array(this.camsEnabled.length).keys()];
   },
 
   methods: {
@@ -123,6 +113,13 @@ export default {
 
     toggleCamera(idx: number) {
       this.camsEnabled[idx] = !this.camsEnabled[idx];
+
+      if(this.camsEnabled[idx]) { //add camera feed
+        this.camsStreaming.push(idx);
+      }
+      else { // remove camera feed
+        this.camsStreaming = this.camsStreaming.filter((x: number) => x != idx);
+      }
     }
 
   }
