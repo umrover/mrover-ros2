a301356aaccfaf6b432f51890be1d3142b10df68
jbrhm <jrabraham1904@gmail.com>
2025-01-29 09:31:12 -0500

    updated tag detector to have main.cpp

--- perception/tag_detector/tag_detector.cpp
+++ perception/tag_detector/tag_detector.cpp
@@ -80,20 +80,4 @@ namespace mrover {
             imageCallback(msg);
         });
     }
-
 } // namespace mrover
-
-auto main(int argc, char** argv) -> int {
-    rclcpp::init(argc, argv);
-
-    auto stereoTD = std::make_shared<mrover::StereoTagDetectorNodelet>();
-    auto imageTD = std::make_shared<mrover::ImageTagDetectorNodelet>();
-
-    rclcpp::executors::SingleThreadedExecutor executor;
-    executor.add_node(stereoTD);
-    executor.add_node(imageTD);
-    executor.spin();
-
-    rclcpp::shutdown();
-    return EXIT_SUCCESS;
-}
