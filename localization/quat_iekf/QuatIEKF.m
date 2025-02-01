33e2e57d205ba9d21ac8de9e40534b370f99803b
Pearl Lin <pearllin@umich.edu>
2025-02-01 09:48:59 -0500

    update and acceleration observation for quaternion iekf

--- localization/quat_iekf/QuatIEKF.m
+++ localization/quat_iekf/QuatIEKF.m
@@ -2,13 +2,33 @@
 
 classdef QuatIEKF < handle
     properties
-        q
-        B
+        g
+        g_err
+
+        % q
+        % B
+        % 
+        % q_err
+        % B_err
+        
         P
        
     end
 
     methods(Static)
+
+        function result = quat_to_rotm(q)
+            q0 = q(1);
+            qv = q(2:end);
+
+            q_v_x = [0 -qv(3) qv(2);
+                     qv(3) 0 -qv(1);
+                     -qv(2) qv(1) 0];
+
+            result = (q0.^2 - qv' * qv) * eye(3) + 2 * qv * qv' - 2 * q0 * q_v_x;
+
+        end
+
         % quat is a 1x4 array representing a quaternion (w x y z]
         function A = A(quat)
 
@@ -19,31 +39,51 @@ classdef QuatIEKF < handle
             A = (q_0.^2 - q_v' * q_v) * eye(3) + 2 * (q_v * q_v') - 2 * q_0 * q_v_x;
         end
 
-        % turn vector in corresponding quaternion representation (scalar
-        % term = 0)
-        function quat = to_quat(v)
-            quat = quaternion([0 v]);
+        function result = quat_multiply(q, p)
+            % disp(q);
+            % disp(p);
+
+            q0 = q(1);
+            qv = q(2:end);
+
+            p0 = p(1);
+            pv = p(2:end);
+
+            % disp("qv");
+            % disp(qv);
+            % disp("pv");
+            % disp(pv);
+
+            scalar_part = q0 * p0 - qv' * pv;
+            vector_part = q0 * pv + p0 * qv + cross(qv, pv);
+
+            result = [scalar_part; vector_part];
+
         end
 
-        % TODO: put vector into multiplicative group form?? idk what this is called - the
-        % superscript x thingy?
-        function v_x = wedge(v)
-            v_x = [0 -v(3) v(2);
+
+
+        function result = quat_conjugate(q)
+
+            result = [q(1); -1 * q(2:end)];
+
+        end
+
+        % skew symmetric matrix of v
+        function result = sup_x(v)
+            result = [0 -v(3) v(2);
                    v(3) 0 -v(1);
                    -v(2) v(1) 0];
         end
 
-        % this is exp subscript p in the paper
-        % TODO: how to incorporate divide by 2?
-        function exp_p = exp_p(rotation_axis_vec)
+        
+        % see eq 4 in paper
+        function result = exp_q(x)
+            scalar_part = cos(norm(x));
+            vector_part = (x / norm(x)) * sin(norm(x));
 
-            q_0 = cos(norm(rotation_axis_vec));
-            q_v = (rotation_axis_vec / norm(rotation_axis_vec) * sin(norm(rotation_axis_vec)));
-
-            % TODO: kind of a bandage solution
-            q_v(isnan(q_v)) = 0;
+            result = [scalar_part; vector_part];
             
-            exp_p = [q_0; q_v];
 
         end
 
@@ -52,83 +92,160 @@ classdef QuatIEKF < handle
 
 
     methods
-        % I-EKF initial values
+        % Q-IEKF initial values
         function obj = QuatIEKF()
 
-            obj.q = quaternion(1,0,0,0);
-            obj.B = zeros(3,1);
-            obj.P = eye(6);
+            % obj.g = [1 0 0 0 0 0 0]';
+            obj.g = [0.0050038, 0.07056, 0.07056, 0.9949962, 0, 0, 0]';
+
+            % see eq 14 in paper
+            obj.g_err = [1 0 0 0 0 0 0]';
+            obj.P = 500 * eye(6);
+           
 
         end
 
-        % w is a 1x3 array of angular velocities (rad/s)
-        % n is a 1x3 array of noise
-        function propogate(obj, w, n, dt)
+        % w is a 3x1 array of angular velocities (rad/s)
+        % n is a 3x1 array of noise
+        function gyro_predict(obj, w, n_v, n_u, dt)
             
-            % construct F, A, Q
-            I_w = QuatIEKF.A(compact(obj.q))' * (w' - obj.B);
-            N_v = diag(n);
 
-            F = [zeros(3,3), -eye(3);
-                 zeros(3,3), QuatIEKF.wedge(I_w)];
-            G = [QuatIEKF.A(compact(obj.q))', zeros(3,3);
-                 zeros(3,3), -QuatIEKF.A(compact(obj.q))'];
-            Q = [N_v, zeros(3,3);
-                 zeros(3,3), zeros(3,3)]; % TODO: for now assuming bias noise is 0
-            
-            % propogate
-            obj.q = 0.5 * obj.q * exp(QuatIEKF.to_quat(((w' - obj.B) * dt)'));
-            obj.P = expm(F * dt) * obj.P * (expm(F * dt))' + expm(G * dt) * Q * (expm(G * dt))'; % TODO: check this error propogation
+            q = obj.g(1:4);
+            B = obj.g(5:7);
+            q_err = obj.g_err(1:4);
+            B_err = obj.g_err(5:7);
+
+            % see eq 21 and 22 of paper
+            n_v_est = QuatIEKF.quat_multiply(QuatIEKF.quat_multiply(q, [0; n_v]), QuatIEKF.quat_conjugate(q));
+            I_w_est = QuatIEKF.quat_multiply(QuatIEKF.quat_multiply(q, [0; w - B]), QuatIEKF.quat_conjugate(q));
+            n_u_est = QuatIEKF.quat_multiply(QuatIEKF.quat_multiply(q, [0; n_u]), QuatIEKF.quat_conjugate(q));
+            % disp("n_u:")
+            % disp(n_u);
+            % disp("I_w_est:");
+            % disp(I_w_est);
+            % disp("n_u_est");
+            % disp(n_u_est);
+
+      
+            obj.g_err(1:4) = q_err + QuatIEKF.quat_multiply(-0.5 * q_err, [0; B_err]) * dt + QuatIEKF.quat_multiply(0.5 * n_v_est, q_err) * dt;
+            obj.g_err(1:4) = quatnormalize(obj.g_err(1:4)')';
+            obj.g_err(5:7) = B_err + cross(QuatIEKF.quat_to_rotm(q_err) * (I_w_est(2:end) - n_u_est(2:end)), B_err) * dt - QuatIEKF.quat_to_rotm(q_err) * n_u_est(2:end) * dt;
+
+
+            % see eq 27 of paper
+            F = [zeros(3), -eye(3);
+                 zeros(3), QuatIEKF.sup_x(I_w_est(2:end))];
+            G = [QuatIEKF.quat_to_rotm(q)', zeros(3);
+                 zeros(3), -QuatIEKF.quat_to_rotm(q)'];
+            Q = diag([n_v; n_u].^2);
+
+            % disp("Q");
+            % disp(Q);
+            % 
+            % disp("GQG:");
+            % disp(G * (expm(F * dt) * Q * dt * expm(F * dt)') * G');
+
+
+            % see https://www.youtube.com/watch?v=WHBgSqRPpu4, eq 33 of
+            % paper
+            obj.P = expm(F * dt) * obj.P * expm(F * dt)' + G * (expm(F * dt) * Q * dt * expm(F * dt)') * G';
+
+            % see https://stackoverflow.com/questions/46908345/integrate-angular-velocity-as-quaternion-rotation
+            obj.g(1:4) = QuatIEKF.quat_multiply(q, QuatIEKF.exp_q((w - B) * dt));
+            obj.g(5:7) = B;
+
+            % disp("propogation amount:");
+            % disp(QuatIEKF.exp_q((w - B) * dt));
+
+            % disp("quat after gyro:");
+            % disp(obj.g(1:4));
+
+
+
+           
+
+            % obj.P = expm(F * dt) * obj.P * (expm(F * dt))' + expm(G * dt) * Q * (expm(G * dt))'; % TODO: check this error propogation
         end
 
-        % m is a 1x3 reference vector in world frame
-        function correct(obj, m, n)
-            % compute H, R, V
-            H = [QuatIEKF.wedge(m) zeros(3,3)];
-            % TODO: think this is supposed to be change in noise? For now
-            % let's test with no noise
-            R_1 = QuatIEKF.A(compact(obj.q))';
-            R_2 = QuatIEKF.A(compact(obj.q));
-            R = R_1 * diag(n) * R_2;
+        % takes in acceleration vector and associated covariance
+        function accel_correct(obj, a, cov_a)
 
-            %disp(R);
+            % see https://matthewhampsey.github.io/blog/2020/07/18/mekf,
+            % and eq 31, 34 of paper
+            H = [QuatIEKF.sup_x([0; 0; -1]), zeros(3)];
+            R = QuatIEKF.quat_to_rotm(obj.g(1:4))' * cov_a * QuatIEKF.quat_to_rotm(obj.g(1:4));
 
-            V = QuatIEKF.A(compact(obj.q))' * n';
+           
+            % disp("R:");
+            % disp(R);
 
-            % compute K, E, C
+            % disp("obj.P * H'");
+            % disp(obj.P * H');
+            % 
+            % disp()
+
+             % see eq 33 of paper
             K = obj.P * H' / (H * obj.P * H' + R);
-            %disp(K);
-            %disp(eye(3) + QuatIEKF.wedge([obj.P(1,1), obj.P(2,2), obj.P(3,3)]));
-            % TODO: sus error
-            %disp(QuatIEKF.wedge([obj.P(1,1), obj.P(2,2), obj.P(3,3)]) * m');
-            errors = [obj.P(1,1); obj.P(2,2); obj.P(3,3); obj.P(4,4); obj.P(5,5); obj.P(6,6)];
-            E = H * errors + V;
 
-            disp(errors);
-            disp(E);
-            %E = m' - (QuatIEKF.wedge([obj.P(1,1), obj.P(2,2), obj.P(3,3)]) * m' + QuatIEKF.A(compact(obj.q))' * n');
-            %disp(E);
-            C = K * E;
-            %disp(K);
+            % disp("P:");
+            % disp(obj.P);
+            % 
+            % disp("PH'");
+            % disp(obj.P * H');
+            % disp("H * obj.P");
+            % disp(H * obj.P);
+            % disp("HPH'");
+            % disp(H * obj.P * H');
+            % disp("HPH' + R");
+            % disp(H * obj.P * H' + R);
+            % disp("R:");
+            % disp(R);
+            % 
+            % disp("K:");
+            % disp(K);
+
+            % innovation, see eq 29 of paper
+            observation = QuatIEKF.quat_multiply(QuatIEKF.quat_multiply(obj.g(1:4), [0; a]), QuatIEKF.quat_conjugate(obj.g(1:4)));
+            predicted = [0; 0; -1];
+            innov = predicted - observation(2:end);
+
+            % disp("predicted");
+            % disp(predicted);
+            % disp("observation:");
+            % disp(observation);
+            % disp("innovation");
+            % disp(innov);
             
-            disp(C);
-            disp(QuatIEKF.exp_p(C(1:3) / 2));
-            temp = vertcat(0, C(1:3));
-            disp(exp(quaternion(temp')));
 
-            % correct
+            % disp("innov:");
+            % disp(innov);
+
+            % see eq 17 of paper
+            correction = K * innov;
+            
+            % disp("correction:")
+            % disp(correction);
+
+            
+
+            % update, see eq 20 of paper
+            obj.g(1:4) = QuatIEKF.quat_multiply(QuatIEKF.exp_q(-0.5 * correction(1:3)), obj.g(1:4));
+            B_update = QuatIEKF.quat_multiply(QuatIEKF.quat_multiply(QuatIEKF.quat_conjugate(obj.g(1:4)), [0; correction(4:6)]), obj.g(1:4));
+            % disp("B update:");
+            % disp(B_update);
+            % 
+            obj.g(5:7) = obj.g(5:7) - B_update(2:end);
+
+            % see eq 33 of paper
             obj.P = (eye(6) - K * H) * obj.P;
-            
-            obj.q = quaternion(QuatIEKF.exp_p(-C(1:3) / 2)') * obj.q;
 
-            %obj.q = exp(-quaternion(QuatIEKF.to_quat(C(1:3)')) / 2) * obj.q;
-            %obj.q = 0.5 * obj.q * exp(quaternion(QuatIEKF.to_quat(w)) * dt);
-            
-            %disp(obj.q' * quaternion(QuatIEKF.to_quat(C(4:end)')) * obj.q);
-            % B_vec = compact(obj.q' * quaternion(QuatIEKF.to_quat(C(4:end)')) * obj.q);
-            % obj.B = obj.B - B_vec(1,2:4)';
+            % disp("P:");
+            % disp(obj.P);
+
 
         end
+
+        
 
         
         
