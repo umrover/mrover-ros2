a2d549f8e224f381d3d0f4eb5e611e549a68de03
jbrhm <jrabraham1904@gmail.com>
2025-02-06 14:54:39 -0500

    added orientation offset

--- teleoperation/basestation_gui/frontend/src/utils.ts
+++ teleoperation/basestation_gui/frontend/src/utils.ts
@@ -33,7 +33,7 @@ const convertDMS = function(coordinate, format: string) {
 const quaternionToMapAngle = function(quaternion: number[]): number {
   const [qx, qy, qz, qw] = quaternion
   const yaw = Math.atan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy * qy + qz * qz))
-  return (yaw) * (180 / Math.PI)
+  return (yaw + Math.PI / 2) * (180 / Math.PI)
 }
 
 export { convertDMS, quaternionToMapAngle }
