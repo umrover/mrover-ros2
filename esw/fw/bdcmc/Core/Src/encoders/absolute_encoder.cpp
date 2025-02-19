11677c3d3e0b3534f7f77cea44f2066f4b21b128
Owen Park <owenpark@umich.edu>
2025-02-18 19:27:44 -0500

    bdcmc encoder and timer fixed

--- esw/fw/bdcmc/Core/Src/encoders/absolute_encoder.cpp
+++ esw/fw/bdcmc/Core/Src/encoders/absolute_encoder.cpp
@@ -45,8 +45,10 @@ namespace mrover {
 
     [[nodiscard]] auto AbsoluteEncoderReader::read() -> std::optional<EncoderReading> {
         if (std::optional<std::uint16_t> counts = try_read_buffer()) {
-            std::uint16_t const timer_tick_current = __HAL_TIM_GET_COUNTER(m_elapsed_timer);
-            Seconds elapsed_time = (1 / CLOCK_FREQ) * (timer_tick_current - m_timer_tick_prev);
+            std::uint32_t const timer_tick_current = __HAL_TIM_GET_COUNTER(m_elapsed_timer);
+            std::uint32_t const tick_diff = timer_tick_current - m_timer_tick_prev;
+            Seconds elapsed_time = (1.0f / CLOCK_FREQ) * static_cast<float>(tick_diff);
+            m_timer_tick_prev = timer_tick_current;
 
             // Absolute encoder returns [0, COUNTS_PER_REVOLUTION)
             // We need to convert this to [-𝜏/2, 𝜏/2)
