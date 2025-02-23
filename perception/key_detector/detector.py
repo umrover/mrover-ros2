41dbfad046385e841782dd58c57c7a908bc9b711
Henry Cheng <cheng201411@gmail.com>
2025-02-23 13:29:56 -0500

    Removed external libs

--- perception/key_detector/detector.py
+++ perception/key_detector/detector.py
@@ -7,23 +7,23 @@ from sensor_msgs.msg import Image as ROSImage
 import time
 
 
-import pickle
-import cv2
-import sys
-import numpy as np
-import cupy as cp
-from ultralytics import YOLO
+
+
+
 
 from keyrover import *
-from keyrover.vision import *
 from keyrover.images.texcoord import *
-from keyrover.images.key_mask import *
+
+
+
 from keyrover.color import NamedPalette
 from keyrover.vision.models import CornersRegressionModel
-from keyrover.datasets import KeyboardCornersDataset
-
+import pickle
+import cv2
+import numpy as np
+from ultralytics import YOLO
+from torchvision.transforms import v2 as transforms
 import torch
-import matplotlib.pyplot as plt
 
 
 SIZE = (256, 256)
@@ -31,26 +31,28 @@ dataset = "v4-nodistort"
 CORNER_REGRESSION_MODEL = "magic-wave-28.pt"
 YOLO_MODEL = f"{MODELS_PATH}/yolo/train4/weights/best.pt"
 
+
+DeviceType = Literal["cpu", "cuda", "mps"]
+if torch.cuda.is_available():
+    device: Final[DeviceType] = "cuda"
+elif torch.backends.mps.is_available():
+    device: Final[DeviceType] = "mps"
+else:
+    device: Final[DeviceType] = "cpu"
+
 class KeyDetector(Node):
     def __init__(self):
         super().__init__('key_detector')
 
         self.get_logger().info("Starting Key Detector Node...")
 
-
-        self.test_dataset = KeyboardCornersDataset([], size=SIZE, version=dataset)
-
         self.corner_regression_model = CornersRegressionModel.load(CORNER_REGRESSION_MODEL)
         self.corner_regression_model.to(device)
         self.corner_regression_model.eval()
 
         self.yolo_segmentation_model = YOLO(YOLO_MODEL)
-
-        output_video_path = 'output_video.mp4'
-        fourcc = cv2.VideoWriter.fourcc(*'XVID')
-        fps=20
-        self.videoWriter = cv2.VideoWriter(output_video_path, fourcc=fourcc, fps=fps, frameSize=(1440, 1920), isColor=True)
-
+        self.yolo_segmentation_model.to(device)
+        self.yolo_segmentation_model.eval()
 
         self.subscription = self.create_subscription(
             ROSImage,
@@ -60,6 +62,8 @@ class KeyDetector(Node):
 
         self.debug_img_pub = self.create_publisher(ROSImage, '/key_detector/img', 1)
 
+        self.resize = transforms.Resize((256, 256))
+
     def imageCallback(self, msg):
         self.get_logger().info("Image Callback...")
         start = time.process_time()
@@ -68,55 +72,40 @@ class KeyDetector(Node):
         img = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
         img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
         img_cv = cv2.resize(img, (640, 480), interpolation = cv2.INTER_LINEAR)
-        img = self.test_dataset.cvt_image(img_cv)
 
-        imageTime = (time.process_time() -start) * 1000
+        img = self.resize(torch.Tensor(img_cv).permute(2, 0, 1))
+
+        imageTime = (time.process_time() - start) * 1000
         start = time.process_time()
         with torch.no_grad():
             # TODO yolo & model to use same input image size
 
             bboxes = self.yolo_segmentation_model.predict(img_cv, conf=0.3, iou=0.3)[0]
-            #print(f'bboxes {bboxes}')
             mask = self.corner_regression_model.predict(img)
 
-            torchTime = (time.process_time() -start) * 1000
-            start = time.process_time()
+        torchTime = (time.process_time() -start) * 1000
+        start = time.process_time()
 
         mask = cv2.resize(mask, bboxes.orig_shape[::-1])
-        #print(f'mask {mask}')
 
-        out = plot_yolo(bboxes, draw_text=False, plot=False)
-        #imshow(out, mask)
-        #describe(mask)
-
-        keys = keys_from_yolo(bboxes)
+        keys = [LabeledBBox(*box.xywh[0], i) for i, box in enumerate(bboxes.boxes)]
         texcoords = TexcoordImage(mask * 255, keys)
 
         with open(f"{DATASETS}/texcoords/{dataset}/texcoords_palette.bin", "rb") as file:
             palette: NamedPalette = pickle.load(file)
-
-        palette.colors
 
         source = np.array(list(texcoords.texcoords.values()))
 
         n = len(source)
         m = len(palette.colors)
 
-        source_nvidia = cp.asarray(source)
-        palette_colors_nvidia = cp.asarray(palette.colors)
-
         def create_distance_matrix(A, B):
             if len(A.shape) == 1:
                 return None
-            #A = cp.repeat(A[:, None, :], len(B), axis=1)
-            return cp.linalg.norm(A[:, None, :] - B[None, :, :], axis=-1)
+            return np.linalg.norm(A[:, None, :] - B[None, :, :], axis=-1)
 
         
-
-        # source (n, 2)-vector of points in 2D
-        # palette (m, 2)-vector of points in 2D
-        # dist_matrix (n, m) matrix of pairwise distance between source & palette
-        distance_matrix = create_distance_matrix(source_nvidia, palette_colors_nvidia)
+        distance_matrix = create_distance_matrix(source, palette.colors)
 
         if(distance_matrix is None):
             '''
@@ -148,55 +137,43 @@ class KeyDetector(Node):
         # m - weight between 1st source & mth target (W1m)
         # ...
         W = distance_cost_matrix.flatten()
-        W = cp.tile(W, 2)
+        W = np.tile(W, 2)
 
-        #print(W.shape)
-
-        targets = cp.tile(palette.colors, (n, 1))
+        targets = np.tile(palette.colors, (n, 1))
         targets = targets.flatten(order="F") * W
 
-        #print(targets.shape)
+        ones = np.ones((n * m, 1))
+        zeros = np.zeros((n * m, 1))
 
-        ones = cp.ones((n * m, 1))
-        zeros = cp.zeros((n * m, 1))
+        repeated_source = np.repeat(source, m, axis=0)
 
-        repeated_source = cp.repeat(source, m, axis=0)
+        P1 = np.hstack([repeated_source, zeros, zeros, ones, zeros])
+        P2 = np.hstack([zeros, zeros, repeated_source, zeros, ones])
+        P = np.vstack([P1, P2]) * W[:, None]
 
-        P1 = cp.hstack([repeated_source, zeros, zeros, ones, zeros])
-        P2 = cp.hstack([zeros, zeros, repeated_source, zeros, ones])
-        P = cp.vstack([P1, P2]) * W[:, None]
+        (a, b, c, d, tx, ty), _, _, _ = np.linalg.lstsq(P, targets, rcond=-1)
+        A = np.array([[a, b, tx], [c, d, ty], [0, 0, 1]])
 
-        (a, b, c, d, tx, ty), _, _, _ = cp.linalg.lstsq(P, targets, rcond=-1)
-        A = cp.array([[a.get(), b.get(), tx.get()], [c.get(), d.get(), ty.get()], [0, 0, 1]])
-
-        ones = cp.ones((n, 1))
-        result = cp.einsum('ij, bj -> bi', A, cp.hstack([source, ones]))
+        ones = np.ones((n, 1))
+        result = np.einsum('ij, bj -> bi', A, np.hstack([source, ones]))
         result = result[:, :2]
 
 
-        cost_matrix = create_distance_matrix(result, palette_colors_nvidia)
+        cost_matrix = create_distance_matrix(result, palette.colors)
 
         cost_matrixTime = (time.process_time() -start) * 1000
 
         from scipy.optimize import linear_sum_assignment
-        row_ind, col_ind = linear_sum_assignment(cost_matrix.get())
+        #Hungarian Algorithm
+        row_ind, col_ind = linear_sum_assignment(cost_matrix)
 
         keys = np.array(list(palette.colors_to_name.values()))
         keys = keys[col_ind]
 
+
+
         scale = 3
-        def plot_prediction():
-            ax = texcoords.scatter()
-            scatter(palette.colors, ax=ax, color="black")
-            scatter(result, ax=ax, color="red")
 
-            for (u, v), cls in palette.colors_to_name.items():
-                ax.text(u - 2, v + 5, cls, c="black", fontsize=5)
-                
-            for (u, v), cls in zip(result, keys):
-                ax.text(u - 2, v - 7, cls, c="red", fontsize=5)
-
-        #plot_prediction()
 
         #img = bboxes.orig_img #.copy()
         width, height, _ = img_cv.shape
