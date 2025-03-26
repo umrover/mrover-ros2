c7f0a5eae58fe9f06b5710454db90cbb0b68c93a
Neven Johnson <neven@umich.edu>
2025-03-26 00:17:55 -0400

    gripper working in sim

--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -59,7 +59,7 @@ namespace mrover {
                         positionsCallback(msg);
                     });
                 };
-                addGroup("arm", {"joint_a", "joint_b", "joint_c", "joint_de_pitch", "joint_de_roll"});
+                addGroup("arm", {"joint_a", "joint_b", "joint_c", "joint_de_pitch", "joint_de_roll", "gripper"});
                 addGroup("drive_left", {"front_left", "middle_left", "back_left"});
                 addGroup("drive_right", {"front_right", "middle_right", "back_right"});
 
@@ -69,6 +69,7 @@ namespace mrover {
                         {"joint_c", "arm_c_link"},
                         {"joint_de_pitch", "arm_d_link"},
                         {"joint_de_roll", "arm_e_link"},
+                        {"gripper", "arm_gripper_link"},
                         {"front_left", "front_left_wheel_link"},
                         {"middle_left", "center_left_wheel_link"},
                         {"back_left", "back_left_wheel_link"},
