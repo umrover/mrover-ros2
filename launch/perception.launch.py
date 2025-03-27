aee8c4b0fe8db7f63793be4907e48e0cf06412e4
John <jrabraham1904@gmail.com>
2025-03-27 10:33:08 -0400

    reformatting percep launch

--- launch/perception.launch.py
+++ launch/perception.launch.py
@@ -12,21 +12,6 @@ from launch.conditions import LaunchConfigurationEquals
 
 
 def generate_launch_description():
-    from pathlib import Path
-
-
-from ament_index_python import get_package_share_directory
-
-import launch
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node, ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
-from launch.conditions import LaunchConfigurationEquals
-
-
-def generate_launch_description():
 
     container = ComposableNodeContainer(
         name="perception",
