0e5c9db5c43ba492cb6a2494195decb8fdf7a82f
John <134429827+jbrhm@users.noreply.github.com>
2025-03-27 18:26:18 -0400

    Tag detector and object detector + update cuda to support 12.6 (#84)
    * need to work on something elsw
    
    * Get blob dimensions from model
    
    * More Verbose
    
    * Better Naming
    
    * Better Naming
    
    * Improved
    
    * Refactored to remove parsing from tensorrt
    
    * Style Checking
    
    * Style Checking
    
    * pre struct
    
    * post model struct
    
    * Works on zed
    
    * Style fixes
    
    * boiler plate
    
    * old code
    
    * removed
    
    * Chunky Camera is just cooked
    
    * Builds
    
    * bruuhhhhh
    
    * Builds
    
    * USB based OD works
    
    * Segfault
    
    * Both OD work at the same tiem
    
    * Tag Detector works in ZED
    
    * How do I view the image with long range cam
    
    * style
    
    * idk
    
    * merged main
    
    * fixed build
    
    * both stereo and usb detectors work
    
    * in sim
    
    * change zed wrapper to nodelet
    
    * Finished Node Composition
    
    * added symlinks
    
    * style
    
    * better launching
    
    * remove message in cmake
    
    * Mutli-Threaded Composition
    
    * removed comment
    
    * fixed formatting
    
    * fixed harcoding seen vector
    
    * intermediate review
    
    * intermediate review
    
    * style
    
    * style
    
    * bump
    
    * vis
    
    * updated tag detector to have main.cpp
    
    * updated cmake
    
    * removed cmake bloat
    
    * changes
    
    * changed percep launch; tested both tag detectors
    
    * everything is tested
    
    * CI
    
    * removed redundancy
    
    * WIP
    
    * undo
    
    * rollback
    
    * Using IPC properly
    
    * Updated Launch File
    
    * Style
    
    * fixing cuda compilation
    
    * fixing cuda compilation
    
    * fixing cuda compilation
    
    * fixing cuda compilation
    
    * fixing cuda compilation
    
    * cuda stuff
    
    * fixing cuda compilation
    
    * added back fuse lld
    
    * formatting
    
    * formatting cmake
    
    * reformatting percep launch
    
    * scripts perms
    
    * sim pub topic
    
    ---------
    
    Co-authored-by: Ali Ryckman <alison.ryckman@gmail.com>
    Co-authored-by: MROVER Perception <umroverperception@gmail.com>

--- perception/object_detector/object_detector.cpp
+++ perception/object_detector/object_detector.cpp
@@ -1,8 +1,10 @@
 #include "object_detector.hpp"
+#include <functional>
 
 namespace mrover {
 
-    ObjectDetectorBase::ObjectDetectorBase() : rclcpp::Node(NODE_NAME), mLoopProfiler{get_logger()} {
+    ObjectDetectorBase::ObjectDetectorBase(rclcpp::NodeOptions const& options) : rclcpp::Node(NODE_NAME, options), mLoopProfiler{get_logger()} {
+        std::string modelName;
 
         std::vector<ParameterWrapper> params{
                 {"camera_frame", mCameraFrame, "zed_left_camera_frame"},
@@ -11,36 +13,60 @@ namespace mrover {
                 {"decrement_weight", mObjDecrementWeight, 1},
                 {"hitcount_threshold", mObjHitThreshold, 5},
                 {"hitcount_max", mObjMaxHitcount, 10},
-                {"model_name", mModelName, "Large-Dataset"},
+                {"model_name", modelName, "Large-Dataset"},
                 {"model_score_threshold", mModelScoreThreshold, 0.75},
-                {"model_nms_threshold", mModelNmsThreshold, 0.5}};
+                {"model_nms_threshold", mModelNMSThreshold, 0.5},
+                {"object_detector_debug", mDebug, true}};
 
         ParameterWrapper::declareParameters(this, params);
 
+        // All of these variables will be invalidated after calling this function
+
         std::filesystem::path packagePath = std::filesystem::path{ament_index_cpp::get_package_prefix("mrover")} / ".." / ".." / "src" / "mrover";
 
-        RCLCPP_INFO_STREAM(get_logger(), "Opening Model " << mModelName);
+        RCLCPP_INFO_STREAM(get_logger(), "Opening Model " << modelName);
 
         RCLCPP_INFO_STREAM(get_logger(), "Found package path " << packagePath);
 
-        mTensorRT = TensortRT{mModelName, packagePath.string()};
+        // Initialize TensorRT Inference Object and Get Important Output Information
+        mTensorRT = TensortRT{modelName, packagePath.string()};
 
-        mDebugImgPub = create_publisher<sensor_msgs::msg::Image>("object_detector/debug_img", 1);
+        using namespace std::placeholders;
 
-        RCLCPP_INFO_STREAM(get_logger(), std::format("Object detector initialized with model: {} and thresholds: {} and {}", mModelName, mModelScoreThreshold, mModelNmsThreshold));
+        mModel = Model(modelName, {0, 0}, {"bottle", "hammer"}, mTensorRT.getInputTensorSize(), mTensorRT.getOutputTensorSize(), [](Model const& model, cv::Mat& rgbImage, cv::Mat& blobSizedImage, cv::Mat& blob) { preprocessYOLOv8Input(model, rgbImage, blobSizedImage, blob); }, [this](Model const& model, cv::Mat& output, std::vector<Detection>& detections) { parseYOLOv8Output(model, output, detections); });
+
+        RCLCPP_INFO_STREAM(get_logger(), std::format("Object detector initialized with model: {} and thresholds: {} and {}", mModel.modelName, mModelScoreThreshold, mModelNMSThreshold));
     }
 
-    StereoObjectDetector::StereoObjectDetector() {
-        mSensorSub = create_subscription<sensor_msgs::msg::PointCloud2>("/zed/left/points", 1, [this](sensor_msgs::msg::PointCloud2::UniquePtr const& msg) {
+    StereoObjectDetector::StereoObjectDetector(rclcpp::NodeOptions const& options) : ObjectDetectorBase(options) {
+        RCLCPP_INFO_STREAM(get_logger(), "Creating Stereo Object Detector...");
+
+        mDebugImgPub = create_publisher<sensor_msgs::msg::Image>("/stereo_object_detector/debug_img", 1);
+
+        mSensorSub = create_subscription<sensor_msgs::msg::PointCloud2>("/zed/left/points", 1, [this](sensor_msgs::msg::PointCloud2::ConstSharedPtr const& msg) {
             StereoObjectDetector::pointCloudCallback(msg);
         });
+    }
+
+    ImageObjectDetector::ImageObjectDetector(rclcpp::NodeOptions const& options) : ObjectDetectorBase(options) {
+        RCLCPP_INFO_STREAM(get_logger(), "Creating Image Object Detector...");
+
+        std::vector<ParameterWrapper> params{
+                {"long_range_camera/fov", mCameraHorizontalFov, 80.0}};
+
+        ParameterWrapper::declareParameters(this, params);
+
+        mDebugImgPub = create_publisher<sensor_msgs::msg::Image>("/long_range_object_detector/debug_img", 1);
+
+        mSensorSub = create_subscription<sensor_msgs::msg::Image>("/long_range_cam/image", 1, [this](sensor_msgs::msg::Image::ConstSharedPtr const& msg) {
+            ImageObjectDetector::imageCallback(msg);
+        });
+
+        mTargetsPub = create_publisher<mrover::msg::ImageTargets>("objects", 1);
     }
 } // namespace mrover
 
 
-auto main(int argc, char** argv) -> int {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<mrover::StereoObjectDetector>());
-    rclcpp::shutdown();
-    return EXIT_SUCCESS;
-}
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(mrover::StereoObjectDetector)
+RCLCPP_COMPONENTS_REGISTER_NODE(mrover::ImageObjectDetector)
