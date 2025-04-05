5b3a0935533f7de4e6dcaef7da27ec2fec973036
Ali Ryckman <alison.ryckman@gmail.com>
2025-04-05 12:11:18 -0400

    cams for science mission

--- launch/jetson_base_cameras.launch.py
+++ launch/jetson_base_cameras.launch.py
@@ -13,61 +13,44 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    container = ComposableNodeContainer(
-        name="cameras",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container_mt",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="mrover",
-                plugin="mrover::GstWebsocketStreamer",
-                name="mob_streamer",
-                parameters=[Path(get_package_share_directory("mrover"), "config", "cameras.yaml")],
-                extra_arguments=[{"use_intra_process_comms": True}],
-            )
-        ],
+    boom_streamer_node = Node(
+        package="mrover",
+        executable="gst_websocket_streamer",
+        name="boom_streamer",
         output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
     )
 
-    # boom_streamer_node = Node(
-    #     package="mrover",
-    #     executable="gst_websocket_streamer",
-    #     name="boom_streamer",
-    #     output="screen",
-    #     parameters=[
-    #         Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-    #     ],
-    # )
+    science_streamer_1_node = Node(
+        package="mrover",
+        executable="gst_websocket_streamer",
+        name="science_streamer_1",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
 
-    # science_streamer_1_node = Node(
-    #     package="mrover",
-    #     executable="gst_websocket_streamer",
-    #     name="science_streamer_1",
-    #     output="screen",
-    #     parameters=[
-    #         Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-    #     ],
-    # )
+    science_streamer_2_node = Node(
+        package="mrover",
+        executable="gst_websocket_streamer",
+        name="science_streamer_2",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
 
-    # science_streamer_2_node = Node(
-    #     package="mrover",
-    #     executable="gst_websocket_streamer",
-    #     name="science_streamer_2",
-    #     output="screen",
-    #     parameters=[
-    #         Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-    #     ],
-    # )
+    ra_streamer_node = Node(
+        package="mrover",
+        executable="gst_websocket_streamer",
+        name="ra_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
 
-    # ra_streamer_node = Node(
-    #     package="mrover",
-    #     executable="gst_websocket_streamer",
-    #     name="ra_streamer",
-    #     output="screen",
-    #     parameters=[
-    #         Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-    #     ],
-    # )
-
-    return LaunchDescription([container])
+    return LaunchDescription([science_streamer_1_node, science_streamer_2_node, ra_streamer_node])
