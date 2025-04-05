9d8ddbccb651572524c565d3f2a4a18c6aad6285
Owen Park <owenmmp@gmail.com>
2025-04-05 12:46:39 -0400

    style and refactor

--- esw/science_hw_bridge.cpp
+++ esw/science_hw_bridge.cpp
@@ -1,5 +1,3 @@
-#include "mrover/srv/detail/enable_bool__struct.hpp"
-#include <cstddef>
 #include <cstdint>
 #include <cstring>
 #include <memory>
@@ -12,15 +10,13 @@
 #include <can_device.hpp>
 #include <messaging_science.hpp>
 #include <parameter.hpp>
-#include <sys/types.h>
 #include <units.hpp>
 
 #include <mrover/msg/can.hpp>
-#include <mrover/msg/oxygen.hpp>
-#include <mrover/msg/uv.hpp>
-#include <mrover/msg/science_thermistors.hpp>
 #include <mrover/msg/heater_data.hpp>
-
+#include <mrover/msg/oxygen.hpp>
+#include <mrover/msg/science_thermistors.hpp>
+#include <mrover/msg/uv.hpp>
 #include <mrover/srv/enable_bool.hpp>
 
 
@@ -31,129 +27,129 @@ namespace mrover {
         B = 1,
     };
 
-    class ScienceBridge final : public rclcpp::Node {
+    class ScienceHWBridge final : public rclcpp::Node {
 
     private:
-        const uint8_t NUMHEATERS = 4;
-        bool uvSensorOnA;
-        bool tempHumiditySensorOnA;
-        bool oxygenSensorOnA;
-        uint8_t heaterStates[4];
-        float thermistorTemps[4];
-        
-        rclcpp::Publisher<sensor_msgs::msg::Temperature>::SharedPtr tempPub;
-        rclcpp::Publisher<sensor_msgs::msg::RelativeHumidity>::SharedPtr humidityPub;
-        rclcpp::Publisher<msg::Oxygen>::SharedPtr oxygenPub;
-        rclcpp::Publisher<msg::UV>::SharedPtr uvPub;
-        rclcpp::Publisher<msg::ScienceThermistors>::SharedPtr thermistorsPub;
-        rclcpp::Publisher<msg::HeaterData>::SharedPtr heaterPub;
+        static constexpr std::uint8_t NUM_HEATERS = 4;
+        bool mUVSensorOnA;
+        bool mTempHumiditySensorOnA;
+        bool mOxygenSensorOnA;
+        std::array<std::uint8_t, 4> mHeaterStates;
+        std::array<float, 4> mThermistorTemps;
 
-        rclcpp::Service<srv::EnableBool>::SharedPtr heaterAutoShutoffSrv;
+        rclcpp::Publisher<sensor_msgs::msg::Temperature>::SharedPtr mTemperaturePub;
+        rclcpp::Publisher<sensor_msgs::msg::RelativeHumidity>::SharedPtr mHumidityPub;
+        rclcpp::Publisher<msg::Oxygen>::SharedPtr mOxygenPub;
+        rclcpp::Publisher<msg::UV>::SharedPtr mUVPub;
+        rclcpp::Publisher<msg::ScienceThermistors>::SharedPtr mThermistorsPub;
+        rclcpp::Publisher<msg::HeaterData>::SharedPtr mHeaterPub;
 
-        rclcpp::Service<srv::EnableBool>::SharedPtr scienceEnableSrvHA0;
-        rclcpp::Service<srv::EnableBool>::SharedPtr scienceEnableSrvHB0;
-        rclcpp::Service<srv::EnableBool>::SharedPtr scienceEnableSrvHA1;
-        rclcpp::Service<srv::EnableBool>::SharedPtr scienceEnableSrvHB1;
-        rclcpp::Service<srv::EnableBool>::SharedPtr scienceEnableSrvWLA;
-        rclcpp::Service<srv::EnableBool>::SharedPtr scienceEnableSrvWLB;
+        rclcpp::Service<srv::EnableBool>::SharedPtr mHeaterAutoShutoffSrv;
 
-        rclcpp::Subscription<msg::CAN>::ConstSharedPtr canSubA;
-        rclcpp::Subscription<msg::CAN>::ConstSharedPtr canSubB;
+        rclcpp::Service<srv::EnableBool>::SharedPtr mScienceEnableSrvHA0;
+        rclcpp::Service<srv::EnableBool>::SharedPtr mScienceEnableSrvHB0;
+        rclcpp::Service<srv::EnableBool>::SharedPtr mScienceEnableSrvHA1;
+        rclcpp::Service<srv::EnableBool>::SharedPtr mScienceEnableSrvHB1;
+        rclcpp::Service<srv::EnableBool>::SharedPtr mScienceEnableSrvWLA;
+        rclcpp::Service<srv::EnableBool>::SharedPtr mScienceEnableSrvWLB;
 
-        CanDevice canDevA;
-        CanDevice canDevB;
+        rclcpp::Subscription<msg::CAN>::ConstSharedPtr mCANSubA;
+        rclcpp::Subscription<msg::CAN>::ConstSharedPtr mCANSubB;
+
+        CanDevice mCANDeviceA;
+        CanDevice mCANDeviceB;
 
         ScienceBoard prevScienceMessage;
 
-        void processHeaterAutoShutoff(const std::shared_ptr<srv::EnableBool::Request> request, std::shared_ptr<srv::EnableBool::Response> response) {
-            canDevA.publish_message(InBoundScienceMessage{HeaterAutoShutOffCommand{.enable_auto_shutoff = request->enable}});
-            canDevB.publish_message(InBoundScienceMessage{HeaterAutoShutOffCommand{.enable_auto_shutoff = request->enable}}); 
+        void processHeaterAutoShutoff(std::shared_ptr<srv::EnableBool::Request> const request, std::shared_ptr<srv::EnableBool::Response> response) {
+            mCANDeviceA.publish_message(InBoundScienceMessage{HeaterAutoShutOffCommand{.enable_auto_shutoff = request->enable}});
+            mCANDeviceB.publish_message(InBoundScienceMessage{HeaterAutoShutOffCommand{.enable_auto_shutoff = request->enable}});
             response->success = true;
             response->message = request->enable ? "Enabled" : "Disabled";
         }
 
-        void processScienceEnable(ScienceBoard scienceBoard, const ScienceDevice dev, const std::shared_ptr<srv::EnableBool::Request> request, std::shared_ptr<srv::EnableBool::Response> response) {
+        void processScienceEnable(ScienceBoard scienceBoard, ScienceDevice const dev, std::shared_ptr<srv::EnableBool::Request> const request, std::shared_ptr<srv::EnableBool::Response> response) {
             if (scienceBoard == ScienceBoard::A)
-                canDevA.publish_message(InBoundScienceMessage{EnableScienceDeviceCommand{.science_device = dev, .enable = request->enable}});
+                mCANDeviceA.publish_message(InBoundScienceMessage{EnableScienceDeviceCommand{.science_device = dev, .enable = request->enable}});
             else
-                canDevB.publish_message(InBoundScienceMessage{EnableScienceDeviceCommand{.science_device = dev, .enable = request->enable}});
-            
+                mCANDeviceB.publish_message(InBoundScienceMessage{EnableScienceDeviceCommand{.science_device = dev, .enable = request->enable}});
+
             response->success = true;
             response->message = request->enable ? "Enabled" : "Disabled";
         }
 
         void processMessage(mrover::HeaterStateData const& message) {
             msg::HeaterData heaterData;
-            heaterData.state.resize(NUMHEATERS);
+            heaterData.state.resize(NUM_HEATERS);
 
-            for (int i = 0; i < NUMHEATERS; i++) {
-                heaterData.state.at(i) = heaterStates[i];
+            for (int i = 0; i < NUM_HEATERS; i++) {
+                heaterData.state.at(i) = mHeaterStates[i];
             }
 
-            for (int i = 0; i < NUMHEATERS / 2; ++i) {
-                int h = prevScienceMessage == ScienceBoard::A ? i : i + (NUMHEATERS / 2);
+            for (int i = 0; i < NUM_HEATERS / 2; ++i) {
+                int h = prevScienceMessage == ScienceBoard::A ? i : i + (NUM_HEATERS / 2);
                 heaterData.state.at(h) = GET_BIT_AT_INDEX(message.heater_state_info.on, i);
-                heaterStates[h] = GET_BIT_AT_INDEX(message.heater_state_info.on, i);
+                mHeaterStates[h] = GET_BIT_AT_INDEX(message.heater_state_info.on, i);
             }
 
-            heaterPub->publish(heaterData);
+            mHeaterPub->publish(heaterData);
         }
 
         void processMessage(mrover::ThermistorData const& message) {
             msg::ScienceThermistors scienceThermistors;
-            scienceThermistors.temps.resize(NUMHEATERS);
+            scienceThermistors.temps.resize(NUM_HEATERS);
 
-            for (int i = 0; i < NUMHEATERS; i++) {
-                scienceThermistors.temps.at(i).temperature = thermistorTemps[i];
+            for (int i = 0; i < NUM_HEATERS; i++) {
+                scienceThermistors.temps.at(i).temperature = mThermistorTemps[i];
             }
 
-            for (int i = 0; i < NUMHEATERS / 2; ++i) {
-                int t = prevScienceMessage == ScienceBoard::A ? i : i + (NUMHEATERS / 2);
+            for (int i = 0; i < NUM_HEATERS / 2; ++i) {
+                int t = prevScienceMessage == ScienceBoard::A ? i : i + (NUM_HEATERS / 2);
                 scienceThermistors.temps.at(t).temperature = message.temps[i];
-                thermistorTemps[t] = message.temps[i];
+                mThermistorTemps[t] = message.temps[i];
             }
 
-            thermistorsPub->publish(scienceThermistors);
+            mThermistorsPub->publish(scienceThermistors);
         }
 
         void processMessage(mrover::SensorData const& message) {
             switch (static_cast<ScienceDataID>(message.id)) {
                 case ScienceDataID::TEMPERATURE: {
-                    if ((prevScienceMessage == ScienceBoard::A && tempHumiditySensorOnA) || (prevScienceMessage == ScienceBoard::B && !tempHumiditySensorOnA)) {
+                    if ((prevScienceMessage == ScienceBoard::A && mTempHumiditySensorOnA) || (prevScienceMessage == ScienceBoard::B && !mTempHumiditySensorOnA)) {
                         sensor_msgs::msg::Temperature msg;
                         msg.temperature = message.data;
                         msg.variance = 0;
-                        tempPub->publish(msg);
+                        mTemperaturePub->publish(msg);
                         break;
                     }
                 }
 
                 case ScienceDataID::HUMIDITY: {
-                    if ((prevScienceMessage == ScienceBoard::A && tempHumiditySensorOnA) || (prevScienceMessage == ScienceBoard::B && !tempHumiditySensorOnA)) {
+                    if ((prevScienceMessage == ScienceBoard::A && mTempHumiditySensorOnA) || (prevScienceMessage == ScienceBoard::B && !mTempHumiditySensorOnA)) {
                         sensor_msgs::msg::RelativeHumidity msg;
                         msg.relative_humidity = message.data;
                         msg.variance = 0;
-                        humidityPub->publish(msg);
+                        mHumidityPub->publish(msg);
                         break;
                     }
                 }
 
                 case ScienceDataID::OXYGEN: {
-                    if ((prevScienceMessage == ScienceBoard::A && oxygenSensorOnA) || (prevScienceMessage == ScienceBoard::B && !tempHumiditySensorOnA)) {
+                    if ((prevScienceMessage == ScienceBoard::A && mOxygenSensorOnA) || (prevScienceMessage == ScienceBoard::B && !mOxygenSensorOnA)) {
                         msg::Oxygen msg;
                         msg.percent = message.data;
                         msg.variance = 0;
-                        oxygenPub->publish(msg);
+                        mOxygenPub->publish(msg);
                         break;
                     }
                 }
 
                 case ScienceDataID::UV: {
-                    if ((prevScienceMessage == ScienceBoard::A && uvSensorOnA) || (prevScienceMessage == ScienceBoard::B && !tempHumiditySensorOnA)) {
+                    if ((prevScienceMessage == ScienceBoard::A && mUVSensorOnA) || (prevScienceMessage == ScienceBoard::B && !mUVSensorOnA)) {
                         msg::UV msg;
                         msg.uv_index = message.data;
                         msg.variance = 0;
-                        uvPub->publish(msg);
+                        mUVPub->publish(msg);
                         break;
                     }
                 }
@@ -165,59 +161,59 @@ namespace mrover {
                 prevScienceMessage = ScienceBoard::A;
             else if (msg->source == "science_b")
                 prevScienceMessage = ScienceBoard::B;
-            
+
             OutBoundScienceMessage const& message = *reinterpret_cast<OutBoundScienceMessage const*>(msg->data.data());
             std::visit([&](auto const& messageAlternative) { processMessage(messageAlternative); }, message);
         }
 
     public:
-        ScienceBridge() : Node{"science_hw_bridge"} {}
+        ScienceHWBridge() : Node{"science_hw_bridge"} {}
 
         void init() {
-            std::vector<ParameterWrapper> parameters = {{"uv_sensor_on_a", uvSensorOnA, true},
-                                                        {"temperature_humidity_sensor_on_a",tempHumiditySensorOnA, true},
-                                                        {"oxygen_sensor_on_a", oxygenSensorOnA, true}};
+            std::vector<ParameterWrapper> parameters = {{"uv_sensor_on_a", mUVSensorOnA, true},
+                                                        {"temperature_humidity_sensor_on_a", mTempHumiditySensorOnA, true},
+                                                        {"oxygen_sensor_on_a", mOxygenSensorOnA, true}};
 
             ParameterWrapper::declareParameters(rclcpp::Node::shared_from_this().get(), parameters);
 
-            for (int i = 0; i < NUMHEATERS; i++) {
-                heaterStates[i] = 0;
-                thermistorTemps[i] = 0;
+            for (int i = 0; i < NUM_HEATERS; i++) {
+                mHeaterStates[i] = 0;
+                mThermistorTemps[i] = 0;
             }
 
-            tempPub = create_publisher<sensor_msgs::msg::Temperature>("science_temperature_data", 10);
-            humidityPub = create_publisher<sensor_msgs::msg::RelativeHumidity>("science_humidity_data", 10);
-            oxygenPub = create_publisher<msg::Oxygen>("science_oxygen_data", 10);
-            uvPub = create_publisher<msg::UV>("science_uv_data", 10);
-            thermistorsPub = create_publisher<msg::ScienceThermistors>("science_thermistors", 10);
-            heaterPub = create_publisher<msg::HeaterData>("science_heater_state", 10);
+            mTemperaturePub = create_publisher<sensor_msgs::msg::Temperature>("science_temperature_data", 10);
+            mHumidityPub = create_publisher<sensor_msgs::msg::RelativeHumidity>("science_humidity_data", 10);
+            mOxygenPub = create_publisher<msg::Oxygen>("science_oxygen_data", 10);
+            mUVPub = create_publisher<msg::UV>("science_uv_data", 10);
+            mThermistorsPub = create_publisher<msg::ScienceThermistors>("science_thermistors", 10);
+            mHeaterPub = create_publisher<msg::HeaterData>("science_heater_state", 10);
 
-            heaterAutoShutoffSrv = create_service<srv::EnableBool>("science_change_heater_auto_shutoff_state", 
-                            [this](srv::EnableBool::Request::SharedPtr const req,srv::EnableBool::Response::SharedPtr res) {processHeaterAutoShutoff(req, res);});
-            
-            scienceEnableSrvHA0 = create_service<srv::EnableBool>("science_enable_heater_a0", 
-                            [this](srv::EnableBool::Request::SharedPtr const req,srv::EnableBool::Response::SharedPtr res) {processScienceEnable(ScienceBoard::A, ScienceDevice::HEATER_0, req, res);});
-                
-            scienceEnableSrvHB0 = create_service<srv::EnableBool>("science_enable_heater_b0", 
-                            [this](srv::EnableBool::Request::SharedPtr const req,srv::EnableBool::Response::SharedPtr res) {processScienceEnable(ScienceBoard::B, ScienceDevice::HEATER_0, req, res);});
+            mHeaterAutoShutoffSrv = create_service<srv::EnableBool>("science_change_heater_auto_shutoff_state",
+                                                                    [this](srv::EnableBool::Request::SharedPtr const req, srv::EnableBool::Response::SharedPtr res) { processHeaterAutoShutoff(req, res); });
 
-            scienceEnableSrvHA1 = create_service<srv::EnableBool>("science_enable_heater_a1", 
-                            [this](srv::EnableBool::Request::SharedPtr const req,srv::EnableBool::Response::SharedPtr res) {processScienceEnable(ScienceBoard::A, ScienceDevice::HEATER_1, req, res);});
+            mScienceEnableSrvHA0 = create_service<srv::EnableBool>("science_enable_heater_a0",
+                                                                   [this](srv::EnableBool::Request::SharedPtr const req, srv::EnableBool::Response::SharedPtr res) { processScienceEnable(ScienceBoard::A, ScienceDevice::HEATER_0, req, res); });
 
-            scienceEnableSrvHB1 = create_service<srv::EnableBool>("science_enable_heater_b1", 
-                            [this](srv::EnableBool::Request::SharedPtr const req,srv::EnableBool::Response::SharedPtr res) {processScienceEnable(ScienceBoard::B, ScienceDevice::HEATER_1, req, res);});
+            mScienceEnableSrvHB0 = create_service<srv::EnableBool>("science_enable_heater_b0",
+                                                                   [this](srv::EnableBool::Request::SharedPtr const req, srv::EnableBool::Response::SharedPtr res) { processScienceEnable(ScienceBoard::B, ScienceDevice::HEATER_0, req, res); });
 
-            scienceEnableSrvWLA = create_service<srv::EnableBool>("science_enable_white_led_a", 
-                            [this](srv::EnableBool::Request::SharedPtr const req,srv::EnableBool::Response::SharedPtr res) {processScienceEnable(ScienceBoard::A, ScienceDevice::WHITE_LED, req, res);});
+            mScienceEnableSrvHA1 = create_service<srv::EnableBool>("science_enable_heater_a1",
+                                                                   [this](srv::EnableBool::Request::SharedPtr const req, srv::EnableBool::Response::SharedPtr res) { processScienceEnable(ScienceBoard::A, ScienceDevice::HEATER_1, req, res); });
 
-            scienceEnableSrvWLB = create_service<srv::EnableBool>("science_enable_white_led_b", 
-                            [this](srv::EnableBool::Request::SharedPtr const req,srv::EnableBool::Response::SharedPtr res) {processScienceEnable(ScienceBoard::B, ScienceDevice::WHITE_LED, req, res);});
-                        
-            canSubA = create_subscription<msg::CAN>("/can/science_a/in", 10, [this](msg::CAN::ConstSharedPtr const& msg) { processCANData(msg); });
-            canSubB = create_subscription<msg::CAN>("/can/science_b/in", 10, [this](msg::CAN::ConstSharedPtr const& msg) { processCANData(msg); });
+            mScienceEnableSrvHB1 = create_service<srv::EnableBool>("science_enable_heater_b1",
+                                                                   [this](srv::EnableBool::Request::SharedPtr const req, srv::EnableBool::Response::SharedPtr res) { processScienceEnable(ScienceBoard::B, ScienceDevice::HEATER_1, req, res); });
 
-            canDevA = CanDevice(rclcpp::Node::shared_from_this(), "jetson", "science_a");
-            canDevB = CanDevice(rclcpp::Node::shared_from_this(), "jetson", "science_b");
+            mScienceEnableSrvWLA = create_service<srv::EnableBool>("science_enable_white_led_a",
+                                                                   [this](srv::EnableBool::Request::SharedPtr const req, srv::EnableBool::Response::SharedPtr res) { processScienceEnable(ScienceBoard::A, ScienceDevice::WHITE_LED, req, res); });
+
+            mScienceEnableSrvWLB = create_service<srv::EnableBool>("science_enable_white_led_b",
+                                                                   [this](srv::EnableBool::Request::SharedPtr const req, srv::EnableBool::Response::SharedPtr res) { processScienceEnable(ScienceBoard::B, ScienceDevice::WHITE_LED, req, res); });
+
+            mCANSubA = create_subscription<msg::CAN>("/can/science_a/in", 10, [this](msg::CAN::ConstSharedPtr const& msg) { processCANData(msg); });
+            mCANSubB = create_subscription<msg::CAN>("/can/science_b/in", 10, [this](msg::CAN::ConstSharedPtr const& msg) { processCANData(msg); });
+
+            mCANDeviceA = CanDevice(rclcpp::Node::shared_from_this(), "jetson", "science_a");
+            mCANDeviceB = CanDevice(rclcpp::Node::shared_from_this(), "jetson", "science_b");
         }
     };
 
@@ -225,7 +221,7 @@ namespace mrover {
 
 auto main(int argc, char** argv) -> int {
     rclcpp::init(argc, argv);
-    auto scienceBridge = std::make_shared<mrover::ScienceBridge>();
+    auto scienceBridge = std::make_shared<mrover::ScienceHWBridge>();
     scienceBridge->init();
     rclcpp::spin(scienceBridge);
     rclcpp::shutdown();
