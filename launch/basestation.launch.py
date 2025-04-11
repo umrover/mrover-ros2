1de97e58b3f54e2bd9fe6925cb06d05052b660c5
TJ Neuenfeldt <tjneuenfeldt@gmail.com>
2025-04-10 20:07:21 -0400

    chromium not working but scripts added

--- launch/basestation.launch.py
+++ launch/basestation.launch.py
@@ -6,11 +6,14 @@ def generate_launch_description():
 
     teleop_backend = Node(package="mrover", executable="gui_backend.sh", name="teleop_backend")
 
-    teleop_frontend = Node(package="mrover", executable="gui_frontend.sh", name="teleop_frontend")
+    teleop_menu = Node(package="mrover", executable="gui_chromium_menu.sh", name="teleop_menu")
+
+    teleop_cameras = Node(package="mrover", executable="gui_chromium_cameras.sh", name="teleop_cameras")
 
     return LaunchDescription(
         [
             teleop_backend,
-            teleop_frontend,
+            teleop_menu,
+            teleop_cameras
         ]
     )
