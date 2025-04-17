ec7a0a05a5eb467aacd9eb4ab5bbeb9ecf98056d
MRover Perception <umroverperception@gmail.com>
2025-04-17 18:43:49 -0400

    more cleanup

--- launch/iekf.launch.py
+++ launch/iekf.launch.py
@@ -12,11 +12,11 @@ from launch.conditions import LaunchConfigurationEquals
 
 
 def generate_launch_description():
-    pose_filter_node = Node(
+    iekf_se3_node = Node(
         package="mrover",
-        executable="pose_filter",
-        name="pose_filter",
+        executable="iekf_se3",
+        name="iekf_se3",
         parameters=[os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml")],
     )
 
-    return LaunchDescription([pose_filter_node])
+    return LaunchDescription([iekf_se3_node])
