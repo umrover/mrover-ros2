5daa7c3b2cbcef8a53a78c9affe9dde18958cd88
kevinjin420 <kevinjin0420@gmail.com>
2025-09-03 19:19:10 -0400

    style fix

--- launch/basestation.launch.py
+++ launch/basestation.launch.py
@@ -1,19 +1,12 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
 
-    teleop_backend = Node(
-        package="mrover", 
-        executable="gui_backend.sh", 
-        name="teleop_backend"
-    )
+    teleop_backend = Node(package="mrover", executable="gui_backend.sh", name="teleop_backend")
 
-    teleop_frontend = Node(
-        package="mrover", 
-        executable="gui_frontend.sh", 
-        name="teleop_frontend"
-    )
+    teleop_frontend = Node(package="mrover", executable="gui_frontend.sh", name="teleop_frontend")
 
     return LaunchDescription(
         [
