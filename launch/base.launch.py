3763e7f6cbf380fed36f0b5598b0288bd7258fbe
John <jrabraham1904@gmail.com>
2025-09-08 18:33:31 -0400

    simulator now launches

---
+++ launch/base.launch.py
@@ -0,0 +1,33 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from launch.conditions import LaunchConfigurationEquals
+
+
+def generate_launch_description():
+
+    # TODO (ali): add localization.launch
+
+    diff_drive_controller_node = Node(
+        package="mrover",
+        executable="differential_drive_controller",
+        name="differential_drive_controller",
+        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "esw.yaml")],
+    )
+
+    superstructure_node = Node(
+        package="mrover",
+        executable="superstructure.py",
+        name="superstructure",
+        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "superstructure.yaml")],
+    )
+
+    led_node = Node(package="mrover", executable="led", name="led")
+
+    return LaunchDescription([diff_drive_controller_node, superstructure_node, led_node])
