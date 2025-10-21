4fc16c18058b92a43ee6f264aa43a6777e04ccb1
John <jrabraham1904@gmail.com>
2025-10-21 19:01:52 -0400

    switch to default to Debug

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 # determine the build profile
-build_profile=RelWithDebInfo
+build_profile=Debug
 
 if [[ "$#" -ne "0" ]]; then
 	if [[ "$#" -eq "1" && ( "$1" == "Release" || "$1" == "RelWithDebInfo" || "$1" == "Debug" ) ]]; then
