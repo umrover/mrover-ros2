34f5d68555ec0df09cabc612701d4d6318dca39d
kevinjin420 <kevinjin0420@gmail.com>
2025-10-26 12:38:19 -0400

    scripts and devview removal

--- teleoperation/basestation_gui/frontend/src/router/index.ts
+++ teleoperation/basestation_gui/frontend/src/router/index.ts
@@ -4,7 +4,6 @@ import DMTask from '../views/DMTask.vue'
 import ESTask from '../views/ESTask.vue'
 import AutonTask from '../views/AutonTask.vue'
 import Rover3D from '../components/Rover3D.vue'
-import DevView from '../views/DevView.vue'
 import SPTask from '../views/SPTask.vue'
 
 
@@ -33,11 +32,6 @@ const routes = [
     path: '/Control',
     name: 'Control',
     component: Rover3D
-  },
-  {
-    path: '/dev',
-    name: 'DevView',
-    component: DevView
   },
   {
     path: '/SPTask',
