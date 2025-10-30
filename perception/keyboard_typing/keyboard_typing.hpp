6b67a979e14b212ba5faeba1f95e6cd6b38316b4
Khush <khushbafna101@gmail.com>
2025-10-29 20:30:54 -0400

    initial commit

---
+++ perception/keyboard_typing/keyboard_typing.hpp
@@ -0,0 +1,10 @@
+#include <rclcpp/node.hpp>
+
+namespace mrover {
+    class KeyboardTypingBase : public rclcpp::Node{
+        private:
+        
+        public:
+        KeyboardTypingBase();
+    };
+}
