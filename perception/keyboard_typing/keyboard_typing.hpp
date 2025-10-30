79a91800383a0c9ece7967703afb519875bf49c0
Khush <khushbafna101@gmail.com>
2025-10-29 21:21:50 -0400

    yaw skeleton code

--- perception/keyboard_typing/keyboard_typing.hpp
+++ perception/keyboard_typing/keyboard_typing.hpp
@@ -1,10 +1,14 @@
-#include <rclcpp/node.hpp>
+#pragma once
 
-namespace mrover {
-    class KeyboardTypingBase : public rclcpp::Node{
-        private:
-        
-        public:
-        KeyboardTypingBase();
-    };
-}
+#include "pch.hpp"
+
+class KeyboardTypingNode : public rclcpp::Node{
+    private:
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr mImageSub;
+    rclcpp::Publisher<geometry_msgs::msg::Quaternion>::SharedPtr mCostMapPub;
+
+    auto yawCallback(sensor_msgs::msg::Image::ConstSharedPtr const& msg) -> void;
+
+    public:
+    explicit KeyboardTypingNode(rclcpp::NodeOptions const& options = rclcpp::NodeOptions());
+};
---
+++ perception/keyboard_typing/pch.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <rclcpp/node.hpp>
+#include <rclcpp/publisher.hpp>
+#include <geometry_msgs/msg/quaternion.hpp>
+#include <rclcpp/subscription.hpp>
+#include <sensor_msgs/msg/image.hpp>
