8e8a00a810d0f2af5267f76f60c62be4170a8c5c
Khush <khushbafna101@gmail.com>
2025-11-02 12:29:09 -0500

    topic names

--- perception/keyboard_typing/keyboard_typing.hpp
+++ perception/keyboard_typing/keyboard_typing.hpp
@@ -4,7 +4,10 @@
 
 class KeyboardTypingNode : public rclcpp::Node{
     private:
+    // Sub to /finger_camera/image topic
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr mImageSub;
+
+    // Can pub to any topic just make the name make sense
     rclcpp::Publisher<geometry_msgs::msg::Quaternion>::SharedPtr mCostMapPub;
 
     auto yawCallback(sensor_msgs::msg::Image::ConstSharedPtr const& msg) -> void;
