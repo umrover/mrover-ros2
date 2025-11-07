d1ddb89e26b3f8cbedb143c1757a2e2df71569bc
kevinjin420 <kevinjin0420@gmail.com>
2025-11-06 23:57:30 -0500

    heading and marker update fix

--- teleoperation/basestation_gui/frontend/src/utils/map.ts
+++ teleoperation/basestation_gui/frontend/src/utils/map.ts
@@ -3,7 +3,7 @@ import type { Quaternion } from '@/types/coordinates';
 const quaternionToMapAngle = function (quaternion: Quaternion): number {
   const { x: qx, y: qy, z: qz, w: qw } = quaternion;
   const yaw = Math.atan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy * qy + qz * qz))
-  return yaw * (180 / Math.PI)
+  return yaw * (180 / Math.PI) + 90
 }
 
 export {quaternionToMapAngle }
