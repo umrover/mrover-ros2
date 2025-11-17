ac3a30f463dc007f09df19dbe4566ea9980e4f5b
Hnsorens <hnsorens@gmail.com>
2025-11-17 11:39:39 -0500

    Created untested dynamixel library (basically just copied and pasted)

---
+++ esw/dynamixel_library/dynamixel.cpp
@@ -0,0 +1,158 @@
+#include "dynamixel.hpp"
+#include <memory>
+
+dynamixel::PortHandler * port_handler;
+dynamixel::PacketHandler * packet_handler;
+
+uint8_t dxl_error = 0;
+uint32_t goal_position = 0;
+int dxl_comm_result = COMM_TX_FAIL;
+
+DynamixelServoNode::DynamixelServoNode()
+  : Node("DynamixelServo")
+{
+  RCLCPP_INFO(this->get_logger(), "Run Servo Code");
+
+  this->declare_parameter("qos_depth", 10);
+  int8_t qos_depth = 0;
+  this->get_parameter("qos_depth", qos_depth);
+
+  const auto QOS_RKL10V =
+    rclcpp::QoS(rclcpp::KeepLast(qos_depth)).reliable().durability_volatile();
+
+  set_position_subscriber_ =
+    this->create_subscription<SetPosition>(
+    "set_position",
+    QOS_RKL10V,
+    [this](const SetPosition::SharedPtr msg) -> void
+    {
+      uint8_t dxl_error = 0;
+
+      // Position Value of X series is 4 byte data.
+      // For AX & MX(1.0) use 2 byte data(uint16_t) for the Position Value.
+      uint32_t goal_position = (unsigned int)msg->position;  // Convert int32 -> uint32
+
+      // Write Goal Position (length : 4 bytes)
+      // When writing 2 byte data to AX / MX(1.0), use write2ByteTxRx() instead.
+      dxl_comm_result =
+      packet_handler->write4ByteTxRx(
+        port_handler,
+        (uint8_t) msg->id,
+        ADDR_GOAL_POSITION,
+        goal_position,
+        &dxl_error
+      );
+
+      if (dxl_comm_result != COMM_SUCCESS) {
+        RCLCPP_INFO(this->get_logger(), "%s", packet_handler->getTxRxResult(dxl_comm_result));
+      } else if (dxl_error != 0) {
+        RCLCPP_INFO(this->get_logger(), "%s", packet_handler->getRxPacketError(dxl_error));
+      } else {
+        RCLCPP_INFO(this->get_logger(), "Set [ID: %d] [Goal Position: %d]", msg->id, msg->position);
+      }
+    }
+    );
+
+  auto get_present_position =
+    [this](
+    const std::shared_ptr<GetPosition::Request> request,
+    std::shared_ptr<GetPosition::Response> response) -> void
+    {
+      // Read Present Position (length : 4 bytes) and Convert uint32 -> int32
+      // When reading 2 byte data from AX / MX(1.0), use read2ByteTxRx() instead.
+      dxl_comm_result = packet_handler->read4ByteTxRx(
+        port_handler,
+        (uint8_t) request->id,
+        ADDR_PRESENT_POSITION,
+        reinterpret_cast<uint32_t *>(&present_position),
+        &dxl_error
+      );
+
+      RCLCPP_INFO(
+        this->get_logger(),
+        "Get [ID: %d] [Present Position: %d]",
+        request->id,
+        present_position
+      );
+
+      response->position = present_position;
+    };
+
+  get_position_server_ = create_service<GetPosition>("get_position", get_present_position);
+}
+
+int setupDynamixel(uint8_t dxl_id)
+{
+  // Use Position Control Mode
+  dxl_comm_result = packet_handler->write1ByteTxRx(
+    port_handler,
+    dxl_id,
+    ADDR_OPERATING_MODE,
+    3,
+    &dxl_error
+  );
+
+  if (dxl_comm_result != COMM_SUCCESS) {
+    RCLCPP_ERROR(rclcpp::get_logger("DynamixelServo"), "Failed to set Position Control Mode.");
+  } else {
+    RCLCPP_INFO(rclcpp::get_logger("DynamixelServo"), "Succeeded to set Position Control Mode.");
+  }
+
+  // Enable Torque of DYNAMIXEL
+  dxl_comm_result = packet_handler->write1ByteTxRx(
+    port_handler,
+    dxl_id,
+    ADDR_TORQUE_ENABLE,
+    1,
+    &dxl_error
+  );
+
+  if (dxl_comm_result != COMM_SUCCESS) {
+    RCLCPP_ERROR(rclcpp::get_logger("DynamixelServo"), "Failed to enable torque.");
+  } else {
+    RCLCPP_INFO(rclcpp::get_logger("DynamixelServo"), "Succeeded to enable torque.");
+  }
+}
+
+int main(int argc, char * argv[])
+{
+  port_handler = dynamixel::PortHandler::getPortHandler(DEVICE_NAME);
+  packet_handler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+
+  // Open Serial Port
+  dxl_comm_result = port_handler->openPort();
+  if (dxl_comm_result == false) {
+    RCLCPP_ERROR(rclcpp::get_logger("DynamixelServo"), "Failed to open the port!");
+    return -1;
+  } else {
+    RCLCPP_INFO(rclcpp::get_logger("DynamixelServo"), "Succeeded to open the port.");
+  }
+
+  // Set the baudrate of the serial port (use DYNAMIXEL Baudrate)
+  dxl_comm_result = port_handler->setBaudRate(BAUDRATE);
+  if (dxl_comm_result == false) {
+    RCLCPP_ERROR(rclcpp::get_logger("DynamixelServo"), "Failed to set the baudrate!");
+    return -1;
+  } else {
+    RCLCPP_INFO(rclcpp::get_logger("DynamixelServo"), "Succeeded to set the baudrate.");
+  }
+
+  setupDynamixel(BROADCAST_ID);
+
+  rclcpp::init(argc, argv);
+
+  auto servo = std::make_shared<DynamixelServoNode>();
+  rclcpp::spin(servo);
+  rclcpp::shutdown();
+
+  // Disable Torque of DYNAMIXEL
+  packet_handler->write1ByteTxRx(
+    port_handler,
+    BROADCAST_ID,
+    ADDR_TORQUE_ENABLE,
+    0,
+    &dxl_error
+  );
+
+  return 0;
+}
--- esw/dynamixel_library/something.cpp
+++
@@ -1,9 +0,0 @@
-#include <dynamixel_sdk/dynamixel_sdk.h>
-#include <dynamixel_sdk/port_handler.h>
-#include <dynamixel_sdk/packet_handler.h>
-
-// Now you can use Dynamixel SDK classes
-using namespace dynamixel;
-
-// Example usage:
-PortHandler *port_handler = PortHandler::getPortHandler("some device");
