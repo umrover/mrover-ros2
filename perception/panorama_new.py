1de03a78e2909e9a9e46ff7911fc079f89fef751
Khush Bafna <khushbafna101@gmail.com>
2026-01-02 17:35:55 -0500

    pano additions

--- perception/panorama_new.py
+++ perception/panorama_new.py
@@ -12,6 +12,7 @@ import tf2_ros
 import sys
 from datetime import datetime
 import numpy as np
+import cv2
 
 # Custom msgs
 from mrover.srv import Panorama
@@ -25,7 +26,6 @@ class Panorama(Node):
         self.pano_srv = self.create_service(Panorama, '/panorama', self.pano_callback)
 
         # Panorama bools
-        self.record_image = False
         self.image_taken = False
 
         # Subscriber list, to be populated once the service is called and then emptied
@@ -35,11 +35,29 @@ class Panorama(Node):
         # Debug image publisher
         self.debug_img_pub = self.create_publisher(Image, "/debug_pano", 1)
 
+        # Image stitching variables
+        self.stitcher = cv2.Stitcher.create(cv2.Stitcher_PANORAMA)
+        self.image_list = []
+        self.cam_fov_deg = 105
+        self.cam_fov_rad = np.radians(self.cam_fov_deg)
+        self.num_rotations = np.ceil(2 * np.pi / self.cam_fov_rad)
+
     def pano_callback(self, _, response):
-        self.record_image = True
+        self.stitcher = cv2.Stitcher.create(cv2.Stitcher_PANORAMA)
 
         # Create subscriber to image 
         self.image_sub = self.create_subscription(Image, "/zed_mini/left/image", self.image_callback, 1)
 
+        for i in range(self.num_rotations):
+            pass
+
     def image_callback(self, msg: Image):
-        pass
+        if not self.image_taken:
+            self.current_img = cv2.cvtColor(
+                np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, 4), cv2.COLOR_RGBA2RGB
+            )
+
+            if self.current_img is not None:
+                self.image_list.append(self.current_img)
+
+            self.image_taken = True
