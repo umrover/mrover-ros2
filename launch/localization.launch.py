f3c53f6bceaa5f99db5ae51e07d17d8eefe89acd
SiddhantRBohra <sidrbohra@gmail.com>
2026-02-04 22:07:22 -0500

    localization launch file updates

--- launch/localization.launch.py
+++ launch/localization.launch.py
@@ -4,67 +4,65 @@ from pathlib import Path
 from ament_index_python import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node, ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
-from launch.conditions import LaunchConfigurationEquals
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-
-    # container = ComposableNodeContainer(
-    #     name="perception",
-    #     namespace="",
-    #     package="rclcpp_components",
-    #     executable="component_container_mt",
-    #     composable_node_descriptions=[
-    #         ComposableNode(
-    #             package="mrover",
-    #             plugin="mrover::ZedWrapper",
-    #             name="zed_component",
-    #             parameters=[Path(get_package_share_directory("mrover"), "config", "zed.yaml")],
-    #             extra_arguments=[{"use_intra_process_comms": True}],
-    #         ),
-    #     ],
-    #     output="screen",
-    # )
-
     rover_gps_driver_node = Node(
         package="mrover",
         executable="rover_gps_driver",
         name="rover_gps_driver",
-        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml")],
-        output="screen"
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "localization.yaml"),
+            Path(get_package_share_directory("mrover"), "config", "reference_coords.yaml"),
+        ],
+        output="screen",
+        respawn=True,
     )
 
     gps_linearization_node = Node(
         package="mrover",
         executable="gps_linearization.py",
         name="gps_linearization",
-        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml")],
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "localization.yaml"),
+            Path(get_package_share_directory("mrover"), "config", "reference_coords.yaml"),
+        ],
+        respawn=True,
     )
 
     heading_filter_node = Node(
         package="mrover",
         executable="heading_filter",
         name="heading_filter",
-        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml")],
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "localization.yaml"),
+            Path(get_package_share_directory("mrover"), "config", "reference_coords.yaml"),
+        ],
+        respawn=True,
     )
 
     iekf_se3_node = Node(
         package="mrover",
         executable="iekf_se3",
         name="iekf_se3",
-        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml")],
-        output="screen"
+        parameters=[
+            os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml"),
+            os.path.join(get_package_share_directory("mrover"), "config", "reference_coords.yaml"),
+        ],
+        output="screen",
+        respawn=True,
     )
 
     pose_filter_node = Node(
         package="mrover",
         executable="pose_filter",
         name="pose_filter",
-        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml")],
+        parameters=[
+            os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml"),
+            os.path.join(get_package_share_directory("mrover"), "config", "reference_coords.yaml"),
+        ],
+        respawn=True,
     )
 
-    return LaunchDescription([rover_gps_driver_node, gps_linearization_node, pose_filter_node])
+    return LaunchDescription([rover_gps_driver_node, gps_linearization_node, pose_filter_node, heading_filter_node])
