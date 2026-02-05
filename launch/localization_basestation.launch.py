c4a1f855cc9eb6aeb11ae3e7cc115b71d6efe7b3
SiddhantRBohra <sidrbohra@gmail.com>
2026-02-04 22:08:07 -0500

    localization basestation launch file update

--- launch/localization_basestation.launch.py
+++ launch/localization_basestation.launch.py
@@ -1,13 +1,8 @@
-import os
-
 from ament_index_python import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node, ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
-from launch.conditions import LaunchConfigurationEquals
+from launch_ros.actions import Node
+from pathlib import Path
 
 
 def generate_launch_description():
@@ -16,7 +11,10 @@ def generate_launch_description():
         package="mrover",
         executable="basestation_gps_driver.py",
         name="basestation_gps_driver",
-        parameters=[os.path.join(get_package_share_directory("mrover"), "config", "localization.yaml")],
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "localization.yaml"),
+            Path(get_package_share_directory("mrover"), "config", "reference_coords.yaml"),
+        ],
     )
 
     return LaunchDescription(
