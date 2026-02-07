3b28f1dbd75f514443e4157be7bc181eb522f3f9
SiddhantRBohra <sidrbohra@gmail.com>
2026-02-07 13:18:48 -0500

    map.ts for heading filter

--- teleoperation/basestation_gui/frontend/src/utils/map.ts
+++ teleoperation/basestation_gui/frontend/src/utils/map.ts
@@ -3,7 +3,9 @@ import type { Quaternion } from '@/types/coordinates';
 const quaternionToMapAngle = function (quaternion: Quaternion): number {
   const { x: qx, y: qy, z: qz, w: qw } = quaternion;
   const yaw = Math.atan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy * qy + qz * qz));
-  return yaw * (180 / Math.PI) + 90
+  const yaw_deg = yaw * (180 / Math.PI);
+  const map_deg = 90 - yaw_deg;
+  return ((map_deg % 360) + 360) % 360;
 };
 
 export { quaternionToMapAngle };
