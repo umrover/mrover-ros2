326f7a034483e269f5686bed135b0a425fa793c1
Khush Bafna <khushbafna101@gmail.com>
2026-02-13 15:24:32 -0500

    de cam transform, need to change

--- launch/autonomy.launch.py
+++ launch/autonomy.launch.py
@@ -50,5 +50,10 @@ def generate_launch_description():
         executable="static_transform_publisher",
         arguments=["0", "0", "1", "0", "0", "0", "1", "base_link", "zed_left_camera_frame"],
     )
+    joint_de_to_cam = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["0", "0", "1", "0", "0", "0", "1", "base_link", "zed_left_camera_frame"], # TODO change
+    )
 
     return LaunchDescription([launch_jetson_base, launch_perception, base_link_to_zed, base_link_to_right_gps])
