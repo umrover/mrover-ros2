0cab04f15d9d61df781dd1d50ed374646659a159
kevinjin420 <kevinjin0420@gmail.com>
2026-02-19 18:26:37 -0500

    fix sensors

--- simulator/simulator.sensors.cpp
+++ simulator/simulator.sensors.cpp
@@ -242,9 +242,9 @@ namespace mrover {
                 for (std::string const& msgName: motorGroup.names) {
                     std::string urdfName = mMsgToUrdf.left.at(msgName);
 
-                    controllerState.name.push_back(msgName);
-                    controllerState.state.emplace_back("Armed");
-                    controllerState.error.emplace_back("None");
+                    controllerState.names.push_back(msgName);
+                    controllerState.states.emplace_back("Armed");
+                    controllerState.errors.emplace_back("None");
                     std::uint8_t limitSwitches = 0b000;
                     if (auto limits = rover.model.getLink(urdfName)->parent_joint->limits) {
                         double jointPosition = rover.physics->getJointPos(rover.linkNameToMeta.at(urdfName).index);
@@ -252,7 +252,7 @@ namespace mrover {
                         if (jointPosition < limits->lower + OFFSET) limitSwitches |= 0b001;
                         if (jointPosition > limits->upper - OFFSET) limitSwitches |= 0b010;
                     }
-                    controllerState.limit_hit.push_back(limitSwitches);
+                    controllerState.limits_hit.push_back(limitSwitches);
 
                     jointState.name.push_back(msgName);
                     jointState.position.push_back(rover.physics->getJointPos(rover.linkNameToMeta.at(urdfName).index));
