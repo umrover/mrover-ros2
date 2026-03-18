1d51e66bc28118ce4a987f06ba8e9d006511510b
Eric D'Urso <edurso@edurso.com>
2026-03-18 12:20:56 -0400

    Add ESW Firmware Submodule (#141)
    * delete esw/fw
    
    * add fw submodule
    
    * start to look at motor_library
    
    * update submodule
    
    * update mjbots/moteus dep
    
    * first pass at dbc impl
    
    * validate BMC functionality from ROS2
    
    * touch up drive controllers
    
    * clang format
    
    * fix mjbots reply protocol
    
    * add arm hw bridge - keep to a + gripper (both tested and ready)
    
    * uprev fw
    
    * clang format
    
    * refactor & style
    
    * joint b position integration, periodically query moteus state when inactive
    
    * send query for all motor transactions, only update limits if they are present
    
    * add joint c, fix joint b triangle math
    
    * style
    
    * de
    
    * cleaned up and fixed incorrect mappings
    
    * restore old file from teleop-starter
    
    * Configurations from filming
    
    * Read DE Pitch Properly from controller
    
    * rename cam to pusher to be compliant to the ESW code
    
    * rename cam to pusher to be compliant to the ESW code
    
    * draft pusher service implementation
    
    * test pusher service
    
    * added science_hw_bridge for sar fliming
    
    * update frontend types to match updated icd
    
    * setup for science
    
    * format
    
    * science board changes
    
    * update from comments
    
    * style
    
    * changed can busses
    
    * added more detailed logging
    
    * working launch file for drive on the jetson
    
    * switch to mrover CAN
    
    * format and uprev submodule
    
    * update joint c config
    
    * checkout esw submodule top-level
    
    * fix esw submodule endpoint
    
    * add joint b offset
    
    * added conditional build for esw repo
    
    * target specific commit for esw submodule
    
    ---------
    
    Co-authored-by: kevinjin420 <kevinjin0420@gmail.com>
    Co-authored-by: John <jrabraham1904@gmail.com>
    Co-authored-by: nbaskins <nbaskins@umich.edu>
    Co-authored-by: UMRover Perception <72796844+umroverPerception@users.noreply.github.com>
    Co-authored-by: umroverPerception <umroverperception@gmail.com>

--- esw/differential_drive_controller.cpp
+++ esw/differential_drive_controller.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <chrono>
 #include <memory>
 #include <string>
 
@@ -17,48 +18,68 @@
 
 namespace mrover {
 
-    std::vector<std::string> LEFT_NAMES{"front_left", "middle_left", "back_left"};
-    std::vector<std::string> RIGHT_NAMES{"front_right", "middle_right", "back_right"};
-
     class DifferentialDriveController final : public rclcpp::Node {
-    public:
-        DifferentialDriveController() : Node{"differential_drive_controller"} {
-            declare_parameter("rover.width", rclcpp::ParameterType::PARAMETER_DOUBLE);
-            declare_parameter("rover.length", rclcpp::ParameterType::PARAMETER_DOUBLE);
-            declare_parameter("rover.wheel_radius", rclcpp::ParameterType::PARAMETER_DOUBLE);
+        struct cmd_t {
+            geometry_msgs::msg::Twist msg;
+            rclcpp::Time stamp;
+            bool valid{false};
+        };
 
-            leftVelocityPub = create_publisher<msg::Velocity>("drive_left_velocity_cmd", 10);
-            rightVelocityPub = create_publisher<msg::Velocity>("drive_right_velocity_cmd", 10);
+        static constexpr RadiansPerSecond MAX_SPEED{70.0};
+        static constexpr auto TIMEOUT = std::chrono::milliseconds(200);
 
-            twistSub = create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10, [this](geometry_msgs::msg::Twist::ConstSharedPtr const& msg) {
-                moveDrive(msg);
-            });
+        std::vector<std::string> const NAMES{"front_left", "middle_left", "back_left", "front_right", "middle_right", "back_right"};
+
+        cmd_t mJoystick, mController, mNavigation;
+        rclcpp::Publisher<msg::Velocity>::SharedPtr mVelocityPub;
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr mJoystickSub, mControllerSub, mNavigationSub;
+        rclcpp::TimerBase::SharedPtr mTimer;
+
+        auto joystickCallback(geometry_msgs::msg::Twist::ConstSharedPtr const& msg) -> void {
+            mJoystick = {*msg, now(), true};
         }
 
-    private:
-        auto moveDrive(geometry_msgs::msg::Twist::ConstSharedPtr const& msg) -> void {
+        auto controllerCallback(geometry_msgs::msg::Twist::ConstSharedPtr const& msg) -> void {
+            mController = {*msg, now(), true};
+        }
+
+        auto navigationCallback(geometry_msgs::msg::Twist::ConstSharedPtr const& msg) -> void {
+            mNavigation = {*msg, now(), true};
+        }
+
+        void update() {
+            auto is_fresh = [&](cmd_t const& c) {
+                return c.valid && (now() - c.stamp) < TIMEOUT;
+            };
+
+            if (is_fresh(mJoystick)) {
+                moveDrive(std::make_shared<geometry_msgs::msg::Twist>(mJoystick.msg));
+            } else if (is_fresh(mController)) {
+                moveDrive(std::make_shared<geometry_msgs::msg::Twist>(mController.msg));
+            } else if (is_fresh(mNavigation)) {
+                moveDrive(std::make_shared<geometry_msgs::msg::Twist>(mNavigation.msg));
+            }
+        }
+
+        auto moveDrive(geometry_msgs::msg::Twist::ConstSharedPtr const& msg) const -> void {
             // See 13.3.1.4 in "Modern Robotics" for the math
             // Link: https://hades.mech.northwestern.edu/images/7/7f/MR.pdf
 
-            Meters roverWidth{get_parameter("rover.width").as_double()};
-            Meters roverLength{get_parameter("rover.length").as_double()};
-            Meters wheelRadius{get_parameter("rover.wheel_radius").as_double()};
+            Meters const roverWidth{get_parameter("rover.width").as_double()};
+            Meters const roverLength{get_parameter("rover.length").as_double()};
+            Meters const wheelRadius{get_parameter("rover.wheel_radius").as_double()};
 
-            compound_unit<Radians, inverse<Meters>> wheelLinearToAngular = Radians{1} / wheelRadius;
+            compound_unit<Radians, inverse<Meters>> const wheelLinearToAngular = Radians{1} / wheelRadius;
 
-            MetersPerSecond forward{msg->linear.x};
-            RadiansPerSecond angular{msg->angular.z};
+            MetersPerSecond const forward{msg->linear.x};
+            RadiansPerSecond const angular{msg->angular.z};
 
-            Meters wheelDistanceInner = roverWidth / 2;
-            Meters wheelDistanceOuter = sqrt(square(roverWidth / 2) + square(roverLength / 2));
-
-            // TODO(quintin):   Don't ask me to explain perfectly why we need to cancel out a meters unit in the numerator
-            //                  I think it comes from the fact that there is a unit vector in the denominator of the equation
+            Meters const wheelDistanceInner = roverWidth / 2;
+            Meters const wheelDistanceOuter = sqrt(square(roverWidth / 2) + square(roverLength / 2));
 
             // The outer wheel needs to cover more ground than the inner wheel, so spin at a higher angular velocity
-
-            MetersPerSecond deltaInner = angular / Radians{1} * wheelDistanceInner;
-            MetersPerSecond deltaOuter = angular / Radians{1} * wheelDistanceOuter;
+            MetersPerSecond const deltaInner = angular / Radians{1} * wheelDistanceInner;
+            MetersPerSecond const deltaOuter = angular / Radians{1} * wheelDistanceOuter;
 
             RadiansPerSecond leftInner = (forward - deltaInner) * wheelLinearToAngular;
             RadiansPerSecond rightInner = (forward + deltaInner) * wheelLinearToAngular;
@@ -67,38 +88,40 @@ namespace mrover {
 
             // It is possible for another node to request an invalid combination of linear and angular velocities that the rover can not realize
             // Instead of clipping, scale down based on the maximal speed to preserve the ratio
-
-            RadiansPerSecond MAX_SPEED{70.0};
-
-            if (RadiansPerSecond maximal = std::max(abs(leftOuter), abs(rightOuter)); maximal > MAX_SPEED) {
-                Dimensionless changeRatio = MAX_SPEED / maximal;
+            if (RadiansPerSecond const maximal = std::max(abs(leftOuter), abs(rightOuter)); maximal > MAX_SPEED) {
+                Dimensionless const changeRatio = MAX_SPEED / maximal;
                 leftInner = leftInner * changeRatio;
                 rightInner = rightInner * changeRatio;
                 leftOuter = leftOuter * changeRatio;
                 rightOuter = rightOuter * changeRatio;
             }
 
-            {
-                msg::Velocity leftVelocity;
-                leftVelocity.names = LEFT_NAMES;
-                leftVelocity.velocities = {leftOuter.get(), leftInner.get(), leftOuter.get()};
-                leftVelocityPub->publish(leftVelocity);
-            }
-            {
-                msg::Velocity rightVelocity;
-                rightVelocity.names = RIGHT_NAMES;
-                rightVelocity.velocities = {rightOuter.get(), rightInner.get(), rightOuter.get()};
-                rightVelocityPub->publish(rightVelocity);
-            }
+            msg::Velocity velocity;
+            velocity.names = NAMES;
+            velocity.velocities = {leftOuter.get(), leftInner.get(), leftOuter.get(), rightOuter.get(), rightInner.get(), rightOuter.get()};
+            mVelocityPub->publish(velocity);
         }
 
-        rclcpp::Publisher<msg::Velocity>::SharedPtr leftVelocityPub, rightVelocityPub;
-        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twistSub;
+    public:
+        DifferentialDriveController() : Node{"differential_drive_controller"} {
+            declare_parameter("rover.width", rclcpp::ParameterType::PARAMETER_DOUBLE);
+            declare_parameter("rover.length", rclcpp::ParameterType::PARAMETER_DOUBLE);
+            declare_parameter("rover.wheel_radius", rclcpp::ParameterType::PARAMETER_DOUBLE);
+
+            mVelocityPub = create_publisher<msg::Velocity>("drive_velocity_cmd", 10);
+
+            mJoystickSub = create_subscription<geometry_msgs::msg::Twist>("/joystick_vel_cmd", 10, [this](geometry_msgs::msg::Twist::ConstSharedPtr const& msg) { joystickCallback(msg); });
+            mControllerSub = create_subscription<geometry_msgs::msg::Twist>("/controller_vel_cmd", 10, [this](geometry_msgs::msg::Twist::ConstSharedPtr const& msg) { controllerCallback(msg); });
+            mNavigationSub = create_subscription<geometry_msgs::msg::Twist>("/nav_vel_cmd", 10, [this](geometry_msgs::msg::Twist::ConstSharedPtr const& msg) { navigationCallback(msg); });
+
+            mTimer = create_wall_timer(std::chrono::milliseconds(20), [this]() -> void { update(); });
+        }
     };
 
 } // namespace mrover
 
-auto main(int argc, char** argv) -> int {
+
+auto main(int const argc, char** argv) -> int {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<mrover::DifferentialDriveController>());
     rclcpp::shutdown();
