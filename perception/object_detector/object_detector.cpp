e90ace07d98fc87ebfe1bbae2a489d2ce7beb299
Khush Bafna <khushbafna101@gmail.com>
2026-03-20 15:53:00 -0400

    object model added

--- perception/object_detector/object_detector.cpp
+++ perception/object_detector/object_detector.cpp
@@ -13,7 +13,7 @@ namespace mrover {
                 {"decrement_weight", mObjDecrementWeight, 1},
                 {"hitcount_threshold", mObjHitThreshold, 5},
                 {"hitcount_max", mObjMaxHitcount, 10},
-                {"model_name", modelName, "Large-Dataset"},
+                {"model_name", modelName, "mock-mission"},
                 {"model_score_threshold", mModelScoreThreshold, 0.75},
                 {"model_nms_threshold", mModelNMSThreshold, 0.5},
                 {"object_detector_debug", mDebug, true}};
