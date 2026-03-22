708ab99b87fd449fd0ca1f6bd8598e1a7009f146
ds12a <dshen1248@gmail.com>
2026-03-22 14:03:45 -0400

    fix potential out-of-bounds issue

--- navigation/smoothing.py
+++ navigation/smoothing.py
@@ -7,8 +7,9 @@ import numpy as np
 import math, sys
 
 
-
-def smoothing(trajectory: Trajectory, context: Context, should_relax: bool, should_interpolate: bool) -> Trajectory:
+def smoothing(
+    trajectory: Trajectory, context: Context, should_relax: bool, should_interpolate: bool
+) -> Trajectory:
     """
     Performs relaxation and/or interpolation on given trajectory
 
@@ -20,9 +21,14 @@ def smoothing(trajectory: Trajectory, context: Context, should_relax: bool, shou
     if should_relax:
         # Perform relaxation
         relaxed_path = Relaxation.relax(
-            context, Trajectory(np.apply_along_axis(lambda coord: cartesian_to_ij(context, coord), 1, trajectory.coordinates))
+            context,
+            Trajectory(
+                np.apply_along_axis(
+                    lambda coord: cartesian_to_ij(context, coord), 1, trajectory.coordinates
+                )
+            ),
         )
-        
+
         # Convert cost map coordinates to cartesian
         cartesian_coords = np.apply_along_axis(
             lambda coord: ij_to_cartesian(context, coord), 1, relaxed_path.coordinates
@@ -33,8 +39,7 @@ def smoothing(trajectory: Trajectory, context: Context, should_relax: bool, shou
 
     else:
         cartesian_coords = trajectory.coordinates
-    
-    
+
     output = Trajectory(cartesian_coords)
     context.publish_path_marker(output.coordinates, [1.0, 0.0, 0.0], "relaxed")
 
@@ -42,7 +47,6 @@ def smoothing(trajectory: Trajectory, context: Context, should_relax: bool, shou
         # Perform spline interpolation
         spline_path = SplineInterpolation.interpolate(context, output, spacing=0.5)
         # plot_spline_path = SplineInterpolation.interpolate(context, output, spacing=0.1)
-
 
         context.publish_path_marker(output.coordinates, [0.0, 1.0, 0.0], "interp")
 
@@ -109,14 +113,20 @@ class Relaxation:
         )
 
         cost = 0.0
+        max_y, max_x = cost_map.height - 1, cost_map.width - 1
+
         for y, x in interpolated:
-            if abs(0.5 - (x - int(x))) < 1e-5 and abs(0.5 - (y - int(y))) < 1e-5:  # Close to grid intersection
+            if (
+                abs(0.5 - (x - int(x))) < 1e-5 and abs(0.5 - (y - int(y))) < 1e-5
+            ):  # Close to grid intersection
                 # Get costs from surrounding cost map squares
+                x, y = int(x), int(y)
+
                 cost_arr = [
-                    cost_map.data[int(x), int(y)],
-                    cost_map.data[int(x) + 1, int(y)],
-                    cost_map.data[int(x), int(y) + 1],
-                    cost_map.data[int(x) + 1, int(y) + 1],
+                    cost_map.data[min(y, max_y), min(x, max_x)],
+                    cost_map.data[min(y + 1, max_y), min(x, max_x)],
+                    cost_map.data[min(y, max_y), min(x + 1, max_x)],
+                    cost_map.data[min(y + 1, max_y), min(x + 1, max_x)],
                 ]
 
                 # Only the minimum cost contributes
@@ -124,14 +134,15 @@ class Relaxation:
             else:
                 # Read cost from corresponding square
                 cost += segment_interval * (
-                    cost_map.data[int(y + 0.5), int(x + 0.5)] + 0.001
+                    cost_map.data[min(int(y + 0.5), max_y), min(int(x + 0.5), max_x)] + 0.001
                 )  # add small number to make cost nonzero so distance matters
+
         return cost
 
     @staticmethod
     def cost_full(ctx: Context, trajectory: Trajectory) -> tuple[float, list[float]]:
         """
-        Returns the total cost of the given trajectory, as tuple containing a float 
+        Returns the total cost of the given trajectory, as tuple containing a float
         and a list of the cost of each individual segment.
         A segment is defined as the cost between two adjacent points in a trajectory.
         """
@@ -153,17 +164,15 @@ class Relaxation:
         Removes the point in the trajectory whose removal results in maximal cost reduction.
         Returns the new trajectory and the new cost.
         """
-        total_cost, segment_costs = Relaxation.cost_full(
-            ctx, trajectory
-        )
+        total_cost, segment_costs = Relaxation.cost_full(ctx, trajectory)
 
         if len(trajectory.coordinates) <= 2:
             return trajectory, total_cost
 
         min_idx = -1
         max_diff = -sys.maxsize
-        
-        # Loops through trajectory nodes, locates the node, that when removed, 
+
+        # Loops through trajectory nodes, locates the node, that when removed,
         # decreases the cost the most (distance is factored into cost).
         for i in range(1, len(trajectory.coordinates) - 1):
             cost_i = Relaxation.cost_segment(ctx, trajectory, i - 1, i + 1)
@@ -194,14 +203,10 @@ class Relaxation:
 
         current_trajectory = trajectory
         candidate_traj, candidate_cost = Relaxation.relax_single(ctx, trajectory)
-        
+
         # Loops until the cost of the relaxed path begins increasing
         # NOTE: adjust rel_tol if the relaxed path is entering high cost areas.
         while candidate_cost <= cost + 0.1 or math.isclose(candidate_cost, cost, rel_tol=0.1):
-
-            manual_new_cost = Relaxation.cost_full(ctx, candidate_traj)
-            # ctx.node.get_logger().info(f"Cost: {cost}, New Cost (relax calc): {candidate_cost}, New Cost (manual calc): {manual_new_cost}")
-
             # If we relax to the point where only the start and end are left, leave the loop
             if len(current_trajectory.coordinates) <= 2:
                 break
@@ -209,25 +214,27 @@ class Relaxation:
             current_trajectory = candidate_traj
             cost = candidate_cost
 
-            # Remove a single point 
+            # Remove a single point
             candidate_traj, candidate_cost = Relaxation.relax_single(ctx, current_trajectory)
 
         # Remove first point in trajectory (rover's current position)
         if len(current_trajectory.coordinates) > 1:
             current_trajectory.coordinates = current_trajectory.coordinates[1:]
-            
+
         return current_trajectory
 
 
 class SplineInterpolation:
     @staticmethod
-    def interpolate(ctx: Context, trajectory: Trajectory, spacing: float = 2.0, k:int = 2) -> Trajectory:
+    def interpolate(
+        ctx: Context, trajectory: Trajectory, spacing: float = 2.0, k: int = 2
+    ) -> Trajectory:
         """
         Fits k-degree splines to the given trajectory and returns a new trajectory with
         evenly spaced points sampled from the splines. We approximate the total distance
         of the calculated splines and map the desired distances of each point to the
         default parameterization produce by scipy.
-        
+
         :param spacing:     Distance between trajectory points on the interpolated path (m)
         :param k:           Degree of the splines used to interpolate between points on the input trajectory
         :return:            Interpolated trajectory with points sampled according to given spacing
@@ -236,7 +243,6 @@ class SplineInterpolation:
         # Return if there are not enough trajectory points to perform interpolation
         if len(trajectory.coordinates) < 2:
             return trajectory
-        
 
         # Calculate unit vector pointing in the trajectory's initial direction
         initial = trajectory.coordinates[1] - trajectory.coordinates[0]
@@ -245,19 +251,25 @@ class SplineInterpolation:
 
         # Inserts point very close after the initial point in the direction of the initial unit vector,
         # forcing spline to follow the initial heading
-        trajectory.coordinates = np.concatenate([
-            trajectory.coordinates[0][np.newaxis, :],
-            (trajectory.coordinates[0] + initial * min(dist, 0.5))[np.newaxis, :],
-            trajectory.coordinates[1:]
-        ], axis=0)
-        
+        trajectory.coordinates = np.concatenate(
+            [
+                trajectory.coordinates[0][np.newaxis, :],
+                (trajectory.coordinates[0] + initial * min(dist, 0.5))[np.newaxis, :],
+                trajectory.coordinates[1:],
+            ],
+            axis=0,
+        )
+
         # Attempts to generate the spline
         try:
-            spline, u = splprep([trajectory.coordinates[:, 0], trajectory.coordinates[:, 1]], s=0, k=2)
+            spline, u = splprep(
+                [trajectory.coordinates[:, 0], trajectory.coordinates[:, 1]], s=0, k=2
+            )
         except Exception as e:
-            print(e)
+            ctx.node.get_logger().error(
+                f"{e} - spline could not be generated, returning original trajectory"
+            )
             return trajectory
-
 
         # Generates a parameterized spline and samples finely along the curve,
         # Selects evenly-spaced points along the spline based on the specified spacing
@@ -273,8 +285,8 @@ class SplineInterpolation:
         samples = int(dist[-1] / spacing)
         target_dist = np.linspace(0, samples * spacing, samples + 1)
 
-        u_spaced = np.interp(target_dist, dist, u_fine)             # map distance to u parameter
-        u_spaced = np.append(u_spaced, u[-1])   # make sure end waypoint is preserved
+        u_spaced = np.interp(target_dist, dist, u_fine)  # map distance to u parameter
+        u_spaced = np.append(u_spaced, u[-1])  # make sure end waypoint is preserved
 
         x_spaced, y_spaced = splev(u_spaced, spline)
 
