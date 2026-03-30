ff60bdda729cb8eab16641bfeab80bd802f31679
ejhong <ejhong@umich.edu>
2026-03-30 19:25:26 -0400

    added pusher service and wait at target:

--- navigation/arm_controller/arm_controller.hpp
+++ navigation/arm_controller/arm_controller.hpp
@@ -7,7 +7,12 @@ namespace mrover {
         struct ArmPos {
             double x{0}, y{0}, z{0}, pitch{0}, roll{0}, gripper{0};
             [[nodiscard]] auto toSE3() const -> SE3d {
-                return SE3d{{x, y, z,}, SO3d{Eigen::Quaterniond{Eigen::AngleAxisd{pitch, R3d::UnitY()} * Eigen::AngleAxisd{roll, R3d::UnitX()}}}};
+                return SE3d{{
+                                    x,
+                                    y,
+                                    z,
+                            },
+                            SO3d{Eigen::Quaterniond{Eigen::AngleAxisd{pitch, R3d::UnitY()} * Eigen::AngleAxisd{roll, R3d::UnitX()}}}};
             }
 
             auto operator+(R3d offset) const -> ArmPos {
@@ -24,49 +29,38 @@ namespace mrover {
             }
         };
 
-
         struct JointWrapper {
             struct JointLimits {
                 double minPos, maxPos, minVel, maxVel;
-                [[nodiscard]] auto posInBounds(double pos) const -> bool {return minPos <= pos && pos <= maxPos;}
-                [[nodiscard]] auto velInBounds(double vel) const -> bool {return minPos <= vel && vel <= maxPos;}
+                [[nodiscard]] auto posInBounds(double pos) const -> bool { return minPos <= pos && pos <= maxPos; }
+                [[nodiscard]] auto velInBounds(double vel) const -> bool { return minPos <= vel && vel <= maxPos; }
             };
-            
+
             JointLimits limits;
             double pos;
         };
-        
-        // TODO: update velocity limits to make them real
+
+        // these positional limits are slightly conservative versions of the limits listed in the 2025-26 cdr
         std::unordered_map<std::string, JointWrapper> joints = {
-            {"joint_a", {
-                .limits = {.minPos = 0, .maxPos = 0.35, .minVel = -0.05, .maxVel = 0.05},
-                .pos = 0
-            }},
-            {"joint_b", {
-                .limits = {.minPos = -0.9, .maxPos = 0, .minVel = -0.05, .maxVel = 0.05},
-                .pos = 0
-            }},
-            {"joint_c", {
-                .limits = {.minPos = -0.959931, .maxPos = 2.87979, .minVel = -0.05 * 2 * std::numbers::pi, .maxVel = 0.05 * 2 * std::numbers::pi},
-                .pos = 0
-            }},
-            {"joint_de_pitch", {
-                .limits = {.minPos = -1.3, .maxPos = 1.2, .minVel = -0.2, .maxVel = 0.2}, // pretty conservative limits atm
-                .pos = 0
-            }},
-            {"joint_de_roll", {
-                .limits = {.minPos = -2.36, .maxPos = 1.44, .minVel = -1, .maxVel = 1},
-                .pos = 0
-            }},
-            {"gripper", {
-                .limits = {.minPos = 0, .maxPos = 0.1, .minVel = -1, .maxVel = 1},
-                .pos = 0
-            }},
+                {"joint_a", {.limits = {.minPos = 0, .maxPos = 0.37, .minVel = -0.05, .maxVel = 0.05}, .pos = 0}},
+                {"joint_b", {.limits = {.minPos = -1.1, .maxPos = 0.25, .minVel = -0.05, .maxVel = 0.05}, .pos = 0}},
+                {"joint_c", {.limits = {.minPos = -1.0, .maxPos = 3.0, .minVel = -0.05 * 2 * std::numbers::pi, .maxVel = 0.05 * 2 * std::numbers::pi}, .pos = 0}},
+                {"joint_de_pitch", {.limits = {.minPos = -1.1, .maxPos = 1.1, .minVel = -0.2, .maxVel = 0.2}, .pos = 0}},
+                {"joint_de_roll", {.limits = {.minPos = -3.14, .maxPos = 3.13, .minVel = -1, .maxVel = 1}, .pos = 0}},
+                {"gripper", {.limits = {.minPos = 0, .maxPos = 0.1, .minVel = -1, .maxVel = 1}, .pos = 0}},
+                {"pusher", {.limits = {.minPos = 0, .maxPos = 0.08, .minVel = -0.1, .maxVel = 0.1}, .pos = 0}},
         };
 
         [[maybe_unused]] rclcpp::Subscription<msg::IK>::SharedPtr mIkSub;
         [[maybe_unused]] rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr mVelSub;
-        [[maybe_unused]] rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr mJointSub;
+        [[maybe_unused]] rclcpp::Subscription<msg::ControllerState>::SharedPtr mJointSub;
+        [[maybe_unused]] rclcpp::Client<srv::Pusher>::SharedPtr mPusherCli;
+
+        rclcpp_action::Server<action::TypingPosition>::SharedPtr mTypingServer;
+        auto handleTypingGoal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const action::TypingPosition_Goal> typingGoal) -> rclcpp_action::GoalResponse;
+        auto handleTypingCancel(std::shared_ptr<rclcpp_action::ServerGoalHandle<action::TypingPosition>> typingGoalHandle) -> rclcpp_action::CancelResponse;
+        auto handleTypingAccepted(std::shared_ptr<rclcpp_action::ServerGoalHandle<action::TypingPosition>> typingGoalHandle) -> void;
+        std::optional<rclcpp_action::GoalUUID> mTypingGoalID;
 
         rclcpp::Publisher<msg::Position>::SharedPtr mPosPub;
         rclcpp::Publisher<msg::Velocity>::SharedPtr mVelPub;
@@ -81,9 +75,16 @@ namespace mrover {
         auto timerCallback() -> void;
 
         ArmPos mArmPos, mTypingOrigin, mPosTarget;
-        std::optional<msg::Position> mPosFallback;
         geometry_msgs::msg::Twist mVelTarget;
         rclcpp::Time mLastUpdate;
+
+        double mCarrotTime = 0.033;
+        double mCarrotk = 1;
+        rclcpp::Time mPrevTime;
+        bool mCarrotInitialized = false;
+        ArmPos mCarrotPos;
+        bool mHold = false;
+        bool mNotInitialized = true;
 
         enum class ArmMode : char {
             VELOCITY_CONTROL,
@@ -91,17 +92,17 @@ namespace mrover {
             TYPING
         };
         ArmMode mArmMode = ArmMode::POSITION_CONTROL;
-        static const rclcpp::Duration TIMEOUT;
+        static rclcpp::Duration const TIMEOUT;
 
     public:
         // TODO(quintin): Neven, please load these from config YAML files instead of hard coding. Ideally they would even be computed at runtime. This way you can change the xacro without worry.
 
         // From: rover.urdf.xacro
         // A is the prismatic joint, B is the first revolute joint, C is the second revolute joint
-        static constexpr double LINK_BC = 0.5344417294;
-        static constexpr double LINK_CD = 0.5531735368;
+        static constexpr double LINK_BC = 0.53271;
+        static constexpr double LINK_CD = 0.39403;
         static constexpr double END_EFFECTOR_LENGTH = 0.20482814; // from CAD
-        static constexpr double JOINT_C_OFFSET = 0.1608485915;
+        static constexpr double JOINT_C_OFFSET = 0.208954;
         static constexpr double JOINT_VEL_THRESH = 0.05;
         static constexpr double MAX_SPEED = 0.1; // in m/s, this is just an estimate
 
@@ -109,7 +110,7 @@ namespace mrover {
 
         void posCallback(msg::IK::ConstSharedPtr const& ik_target);
         void velCallback(geometry_msgs::msg::Twist::ConstSharedPtr const& ik_vel);
-        void fkCallback(sensor_msgs::msg::JointState::ConstSharedPtr const& joint_state);
+        void fkCallback(mrover::msg::ControllerState::ConstSharedPtr const& joint_state);
         auto modeCallback(srv::IkMode::Request::ConstSharedPtr const& req, srv::IkMode::Response::SharedPtr const& resp) -> void;
     };
 
