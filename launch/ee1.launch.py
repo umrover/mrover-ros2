7551ae9dcd617410f8ff766a9d4d240e75b37ca8
umroverPerception <umroverperception@gmail.com>
2026-03-31 20:59:42 -0400

    testing changes

---
+++ launch/ee1.launch.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    ee1_streamer_node = Node(
+        package="mrover",
+        executable="gst_camera_server",
+        name="ee1_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            ee1_streamer_node,
+        ]
+    )
