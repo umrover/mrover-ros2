4f0c8f18b43fdc52927a1a4a3e92a07a498b4615
nbaskins <nbaskins@umich.edu>
2026-04-07 21:02:48 -0400

    added zed_mini changes

--- launch/jetson_base.launch.py
+++ launch/jetson_base.launch.py
@@ -10,55 +10,28 @@ from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
-
-    launch_include_can = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            Path(get_package_share_directory("mrover"), "launch/jetson_can.launch.py").__str__()
-        )
-    )
-
-    diff_drive_controller_node = Node(
-        package="mrover",
-        executable="differential_drive_controller",
-        name="differential_drive_controller",
-        parameters=[Path(get_package_share_directory("mrover"), "config", "esw.yaml")],
-    )
-
-    superstructure_node = Node(
-        package="mrover",
-        executable="superstructure.py",
-        name="superstructure",
-        parameters=[Path(get_package_share_directory("mrover"), "config", "superstructure.yaml")],
-    )
-
-    drive_hw_bridge_node = Node(
-        package="mrover",
-        executable="drive_hw_bridge",
-        name="drive_hw_bridge",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "esw.yaml"),
-            Path(get_package_share_directory("mrover"), "config", "drive.yaml"),
+    zed_mini_container = ComposableNodeContainer(
+        name="zed_mini_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container_mt",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="mrover",
+                plugin="mrover::ZedWrapper",
+                name="zed_mini_wrapper",
+                parameters=[Path(get_package_share_directory("mrover"), "config", "zed_mini.yaml")],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
+            ComposableNode(
+                package="mrover",
+                plugin="mrover::GstCameraServer",
+                name="zed_mini_streamer",
+                parameters=[Path(get_package_share_directory("mrover"), "config", "cameras.yaml")],
+                extra_arguments=[{"use_intra_process_comms": True}],
+            ),
         ],
-    )
-
-    mob_left_streamer_node = Node(
-        package="mrover",
-        executable="gst_camera_server",
-        name="mob_left_streamer",
         output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-        ],
-    )
-
-    static_streamer_node = Node(
-        package="mrover",
-        executable="gst_camera_server",
-        name="static_streamer",
-        output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-        ],
     )
 
     zed_container = ComposableNodeContainer(
@@ -85,24 +58,9 @@ def generate_launch_description():
         output="screen",
     )
 
-    mast_gimbal_hw_bridge = Node(
-        package="mrover",
-        executable="mast_gimbal_hw_bridge",
-        name="mast_gimbal_hw_bridge",
-        output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "mast_gimbal.yaml"),
-        ],
-    )
-
     return LaunchDescription(
         [
-            launch_include_can,
-            diff_drive_controller_node,
-            superstructure_node,
-            drive_hw_bridge_node,
-            mob_left_streamer_node,
-            static_streamer_node,
+            zed_mini_container,
             zed_container,
         ]
     )
