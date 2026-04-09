404fcd44557d01d6147b7abb9a4e11b62c1abc44
umroverPerception <umroverperception@gmail.com>
2026-04-08 20:42:11 -0400

    added camera changes

--- launch/jetson_base.launch.py
+++ launch/jetson_base.launch.py
@@ -10,6 +10,116 @@ from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
+    launch_include_can = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            Path(get_package_share_directory("mrover"), "launch/jetson_can.launch.py").__str__()
+        )
+    )
+
+    diff_drive_controller_node = Node(
+        package="mrover",
+        executable="differential_drive_controller",
+        name="differential_drive_controller",
+        parameters=[Path(get_package_share_directory("mrover"), "config", "esw.yaml")],
+    )
+
+    superstructure_node = Node(
+        package="mrover",
+        executable="superstructure.py",
+        name="superstructure",
+        parameters=[Path(get_package_share_directory("mrover"), "config", "superstructure.yaml")],
+    )
+
+    drive_hw_bridge_node = Node(
+        package="mrover",
+        executable="drive_hw_bridge",
+        name="drive_hw_bridge",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "esw.yaml"),
+            Path(get_package_share_directory("mrover"), "config", "drive.yaml"),
+        ],
+    )
+
+    mob_streamer_node = Node(
+        package="mrover",
+        executable="gst_camera_server",
+        name="mob_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
+
+    joint_a_streamer_node = Node(
+        package="mrover",
+        executable="gst_camera_server",
+        name="joint_a_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
+
+    ee1_streamer_node = Node(
+        package="mrover",
+        executable="gst_camera_server",
+        name="ee1_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
+
+    ee2_streamer_node = Node(
+        package="mrover",
+        executable="gst_camera_server",
+        name="ee2_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
+
+    ee3_streamer_node = Node(
+        package="mrover",
+        executable="gst_camera_server",
+        name="ee3_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
+
+    sp1_streamer_node = Node(
+        package="mrover",
+        executable="gst_camera_server",
+        name="sp1_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
+
+    sp2_streamer_node = Node(
+        package="mrover",
+        executable="gst_camera_server",
+        name="sp2_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
+
+    mob_streamer_node = Node(
+        package="mrover",
+        executable="gst_camera_server",
+        name="mob_streamer",
+        output="screen",
+        parameters=[
+            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
+        ],
+    )
+
     zed_mini_container = ComposableNodeContainer(
         name="zed_mini_container",
         namespace="",
@@ -60,6 +170,17 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            launch_include_can,
+            diff_drive_controller_node,
+            superstructure_node,
+            drive_hw_bridge_node,
+            mob_streamer_node,
+            joint_a_streamer_node,
+            ee1_streamer_node,
+            ee2_streamer_node,
+            ee3_streamer_node,
+            sp1_streamer_node,
+            sp2_streamer_node,
             zed_mini_container,
             zed_container,
         ]
