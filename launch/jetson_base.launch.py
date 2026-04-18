5d707b23c29cc04047f7acab7ef11c40c3c90672
umroverPerception <umroverperception@gmail.com>
2026-04-17 23:59:14 -0400

    updated launch files

--- launch/jetson_base.launch.py
+++ launch/jetson_base.launch.py
@@ -50,76 +50,6 @@ def generate_launch_description():
         ],
     )
 
-    joint_a_streamer_node = Node(
-        package="mrover",
-        executable="gst_camera_server",
-        name="joint_a_streamer",
-        output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-        ],
-    )
-
-    ee1_streamer_node = Node(
-        package="mrover",
-        executable="gst_camera_server",
-        name="ee1_streamer",
-        output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-        ],
-    )
-
-    ee2_streamer_node = Node(
-        package="mrover",
-        executable="gst_camera_server",
-        name="ee2_streamer",
-        output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-        ],
-    )
-
-    ee3_streamer_node = Node(
-        package="mrover",
-        executable="gst_camera_server",
-        name="ee3_streamer",
-        output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-        ],
-    )
-
-    sp1_streamer_node = Node(
-        package="mrover",
-        executable="gst_camera_server",
-        name="sp1_streamer",
-        output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-        ],
-    )
-
-    sp2_streamer_node = Node(
-        package="mrover",
-        executable="gst_camera_server",
-        name="sp2_streamer",
-        output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-        ],
-    )
-
-    mob_streamer_node = Node(
-        package="mrover",
-        executable="gst_camera_server",
-        name="mob_streamer",
-        output="screen",
-        parameters=[
-            Path(get_package_share_directory("mrover"), "config", "cameras.yaml"),
-        ],
-    )
-
     zed_mini_container = ComposableNodeContainer(
         name="zed_mini_container",
         namespace="",
@@ -175,13 +105,7 @@ def generate_launch_description():
             superstructure_node,
             drive_hw_bridge_node,
             mob_streamer_node,
-            joint_a_streamer_node,
-            ee1_streamer_node,
-            ee2_streamer_node,
-            ee3_streamer_node,
-            sp1_streamer_node,
-            sp2_streamer_node,
             zed_mini_container,
-            zed_container,
+            zed_container
         ]
     )
