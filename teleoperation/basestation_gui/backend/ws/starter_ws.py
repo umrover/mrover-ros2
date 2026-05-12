e6d99ffd2791717c16e4db40caf25feb263f109a
kmaglo-um <kmaglo@umich.edu>
2026-05-12 15:50:45 -0400

    Cleanup

--- teleoperation/basestation_gui/backend/ws/starter_ws.py
+++ teleoperation/basestation_gui/backend/ws/starter_ws.py
@@ -2,6 +2,7 @@ from backend.ws.base_ws import WebSocketHandler
 from backend.managers.ros import get_logger
 from rclpy.publisher import Publisher
 from std_msgs.msg import String
+
 class StarterHandler(WebSocketHandler):
     foo_pub: Publisher
 
@@ -9,11 +10,16 @@ class StarterHandler(WebSocketHandler):
         super().__init__(websocket, 'starter')
     
     async def setup(self):
+        # Create publisher
         self.foo_pub = self.node.create_publisher(String, "/foo", 1)
 
+        # Add to list of publishers
         self.publishers.extend([self.foo_pub])
+
+        # Forward published topic to frontend
         self.forward_ros_topic("/foo", String, "foo")
 
+        # Do timer_callback() every 2 seconds
         timer_period = 2
         self.timer = self.node.create_timer(timer_period, self.timer_callback)
         self.i = 0
@@ -22,12 +28,9 @@ class StarterHandler(WebSocketHandler):
         msg = String()
         msg.data = 'Foo has printed ' + str(self.i) + ' times.'
         self.foo_pub.publish(msg)
-        # self.node.get_logger().warning('Publishing: ' + str(msg.data))
         self.i += 1
 
     async def handle_message(self, data):
-        # self.foo_pub.publish("Echo")
-
         msg_type = data.get("type")
 
         if msg_type == "debug":
