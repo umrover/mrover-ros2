12de3a48f82b5e45829dc66ff8d18f1a18af4bd5
John <jrabraham1904@gmail.com>
2026-05-25 10:18:58 -0700

    copy screenshots

--- scripts/pano-copy.sh
+++ scripts/pano-copy.sh
@@ -12,6 +12,7 @@ scp -r jetson:/home/mrover/ros2_ws/src/mrover/data/raw-pano-images $FOLDER
 now_secs=$(date +%s)
 today=$(date +%Y-%m-%d)
 
+# copy all of the downloaded graphs
 find ~/Downloads -type f \( -name "*.png" -o -name "*.txt" \) | while read -r f; do
     base=$(basename "$f")
 
@@ -21,6 +22,30 @@ find ~/Downloads -type f \( -name "*.png" -o -name "*.txt" \) | while read -r f;
         ss="${BASH_REMATCH[3]}"
 
         file_secs=$(date -d "$today $hh:$mm:$ss" +%s)
+
+        diff=$(( now_secs - file_secs ))
+
+        if (( diff >= 0 && diff <= 2700 )); then
+            echo "Copying: $f"
+            cp "$f" "$FOLDER/"
+        fi
+    fi
+done
+
+# copy all of the screenshots
+find ~/Pictures/Screenshots -type f -name "*.png" | while read -r f; do
+    base=$(basename "$f")
+
+    if [[ $base =~ ([0-9]{4})-([0-9]{2})-([0-9]{2})\ ([0-9]{2})-([0-9]{2})-([0-9]{2})\.png$ ]]; then
+        yr="${BASH_REMATCH[1]}"
+        mn="${BASH_REMATCH[2]}"
+        dy="${BASH_REMATCH[3]}"
+
+        hh="${BASH_REMATCH[4]}"
+        mm="${BASH_REMATCH[5]}"
+        ss="${BASH_REMATCH[6]}"
+
+        file_secs=$(date -d "$yr-$mn-$dy $hh:$mm:$ss" +%s)
 
         diff=$(( now_secs - file_secs ))
 
