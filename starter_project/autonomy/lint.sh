4b32667fe14264ffd76be9e8345cbe9db127572e
kevinjin420 <kevinjin0420@gmail.com>
2026-06-17 11:14:18 -0700

    remove pyright remnants from toolchain branch

--- starter_project/autonomy/lint.sh
+++ starter_project/autonomy/lint.sh
@@ -26,8 +26,8 @@ function find_executable() {
   echo "${path}"
 }
 
-readonly BASEDPYRIGHT_PATH=$(find_executable basedpyright "")
+readonly MYPY_PATH=$(find_executable mypy 1.5.1)
 
 echo
-echo "Type checking Python with basedpyright ..."
-"${BASEDPYRIGHT_PATH}" ./src --level error
+echo "Style checking Python with mypy ..."
+"${MYPY_PATH}" --config-file=mypy.ini --check ./src
