2d74f7aa433b2a9be6a391c5f4d624e54d2f93e6
kevinjin420 <kevinjin0420@gmail.com>
2026-06-17 15:28:31 -0700

    fix native CI: explicit Dawn_DIR, skip apt upgrade

--- build_ci.sh
+++ build_ci.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+REPO_ROOT=$(pwd)
+
 # Build in the colcon workspace, not the package
 pushd ../..
 
@@ -11,7 +13,10 @@ export CXX=clang++
 
 # TODO (ali): add build configs for debug vs release
 colcon build \
-	--cmake-args -G Ninja -W no-dev -D CMAKE_BUILD_TYPE=Release -D MROVER_CI=ON \
+	--cmake-args -G Ninja -W no-dev \
+		-D CMAKE_BUILD_TYPE=Release \
+		-D MROVER_CI=ON \
+		-D Dawn_DIR="${REPO_ROOT}/deps/dawn-prebuilt/lib/cmake/Dawn" \
 	--symlink-install \
 	--event-handlers console_direct+ \
 	"$@"
