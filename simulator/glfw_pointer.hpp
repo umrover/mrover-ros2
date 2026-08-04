25716198da4382e2382a18944bba93c9c9a34b6a
kevinjin0420 <kevinjin0420@gmail.com>
2026-08-03 21:46:02 -0700

    named deleter for glfw pointer fix

--- simulator/glfw_pointer.hpp
+++ simulator/glfw_pointer.hpp
@@ -4,9 +4,14 @@
 
 namespace mrover {
 
+    template<typename T, auto Deleter>
+    struct GlfwDeleter {
+        auto operator()(T* p) const -> void { Deleter(p); }
+    };
+
     template<typename T, auto Creater, auto Deleter>
     class GlfwPointer {
-        std::unique_ptr<T, decltype([](auto* p) { Deleter(p); })> mPointer;
+        std::unique_ptr<T, GlfwDeleter<T, Deleter>> mPointer;
 
         static auto check(T* result) -> T* {
             if (!result) throw std::runtime_error("GLFW Error");
