26fd5fe661a37629d0b1f0b560a89ddefb84b883
kevinjin0420 <kevinjin0420@gmail.com>
2026-08-26 20:14:19 -0400

    ansible.sh polish

--- ansible.sh
+++ ansible.sh
@@ -7,18 +7,21 @@ if [ "$#" -le 0 ]; then
     exit 1
 fi
 
-sudo -v # Ensure Ansible has sudo permission
+sudo -v || {
+    echo "Incorrect password." >&2
+    exit 1
+} # validate sudo password up front
 
 readonly MROVER_PATH=$(dirname "$(realpath "$0")")
 
 run_playbook() {
-    ansible-playbook -i "localhost," -c local --ask-become-pass "${MROVER_PATH}"/ansible/"$1" --extra-vars "mrover_repo=${MROVER_PATH}" ${2:+"$2"}
+    ansible-playbook -i "localhost," -c local -K "${MROVER_PATH}"/ansible/"$1" --extra-vars "mrover_repo=${MROVER_PATH}" ${2:+"$2"}
 }
 
-# works around a first-run-only ansible-core "No serialization profile" flake
-# the cause of this is unconfirmed, there are multiple sources online such as 
+# workaround for a first-run-only ansible "No serialization profile" bug
+# the cause of this is unconfirmed, there are multiple sources online such as
 # https://github.com/ansible/ansible/issues/84782 that does not provide a clean solution
-# re-trying the playbook always works to resolve the issue, so temporary fix is below
+# retrying the playbook always works to resolve the issue, so temporary fix is below
 if ! run_playbook "$@"; then
     echo "Ansible run failed; retrying once to counter the known first-run cache flake ..." >&2
     run_playbook "$@"
