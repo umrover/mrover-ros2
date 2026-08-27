3bfc4cc644692d7ee36cdf31ccdce44f5bbfa787
kevinjin0420 <kevinjin0420@gmail.com>
2026-08-27 11:56:35 -0400

    ansible var fix

--- ansible.sh
+++ ansible.sh
@@ -7,10 +7,11 @@ if [ "$#" -le 0 ]; then
     exit 1
 fi
 
-readonly MROVER_PATH=$(dirname "$(realpath "$0")")
+readonly MROVER_PATH=$(dirname "$0")
+readonly ROS2_WS_PATH=$(realpath "${MROVER_PATH}"/../..)
 
 run_playbook() {
-    ansible-playbook -i "localhost," -c local -K "${MROVER_PATH}"/ansible/"$1" --extra-vars "mrover_repo=${MROVER_PATH}" ${2:+"$2"}
+    ansible-playbook -i "localhost," -c local -K "${MROVER_PATH}"/ansible/"$1" --extra-vars "ros2_workspace=${ROS2_WS_PATH} $2"
 }
 
 # workaround for a first-run-only ansible "No serialization profile" bug
