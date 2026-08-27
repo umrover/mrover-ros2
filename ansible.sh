9e35cadbe87a45fa0b6e8a5e9f6f14f4e387ca19
kevinjin0420 <kevinjin0420@gmail.com>
2026-08-26 20:52:51 -0400

    QoL changes out of ubuntu 24

--- ansible.sh
+++ ansible.sh
@@ -7,8 +7,17 @@ if [ "$#" -le 0 ]; then
     exit 1
 fi
 
-sudo -v # Ensure Ansible has sudo permission
+readonly MROVER_PATH=$(dirname "$(realpath "$0")")
 
-readonly MROVER_PATH=$(dirname "$0")
-readonly ROS2_WS_PATH=$(realpath "${MROVER_PATH}"/../..)
-ansible-playbook -i "localhost," -c local "${MROVER_PATH}"/ansible/"$1" --extra-vars "ros2_workspace=${ROS2_WS_PATH} $2"
+run_playbook() {
+    ansible-playbook -i "localhost," -c local -K "${MROVER_PATH}"/ansible/"$1" --extra-vars "mrover_repo=${MROVER_PATH}" ${2:+"$2"}
+}
+
+# workaround for a first-run-only ansible "No serialization profile" bug
+# the cause of this is unconfirmed, there are multiple sources online such as
+# https://github.com/ansible/ansible/issues/84782 that does not provide a clean solution
+# retrying the playbook always works to resolve the issue, so temporary fix is below
+if ! run_playbook "$@"; then
+    echo "Ansible run failed; retrying once to counter the known first-run cache flake ..." >&2
+    run_playbook "$@"
+fi
