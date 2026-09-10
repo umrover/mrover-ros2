47d1ac5585e9f5d768f48d4dc5df712742876b53
Ethan Jhong <ejhon1116@gmail.com>
2026-09-10 01:41:38 -0400

    upgrade to python3.12

--- ansible/roles/jetson_build/files/scripts/build_opencv.sh
+++ ansible/roles/jetson_build/files/scripts/build_opencv.sh
@@ -13,10 +13,9 @@ echo "** Install requirement (1/4)"
 echo "------------------------------------"
 sudo apt-get update
 sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev python3.10-dev python3-numpy
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev python3.12-dev python3-numpy
 sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libv4l-dev v4l-utils qv4l2
 sudo apt-get install -y curl
-
 
 echo "------------------------------------"
 echo "** Download opencv ${version} (2/4)"
@@ -30,7 +29,6 @@ unzip opencv_contrib-${version}.zip
 rm opencv-${version}.zip opencv_contrib-${version}.zip
 cd opencv-${version}/
 
-
 echo "------------------------------------"
 echo "** Build opencv ${version} (3/4)"
 echo "------------------------------------"
@@ -39,16 +37,14 @@ cd release/
 cmake -D WITH_CUDA=ON -D WITH_CUDNN=ON -D CUDA_ARCH_BIN="8.7" -D CUDA_ARCH_PTX="" -D OPENCV_GENERATE_PKGCONFIG=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make "-j$(nproc)"
 
-
 echo "------------------------------------"
 echo "** Install opencv ${version} (4/4)"
 echo "------------------------------------"
 sudo make install
-echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
-echo "export PYTHONPATH=/usr/local/lib/python3.10/site-packages/:$PYTHONPATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >>~/.bashrc
+echo "export PYTHONPATH=/usr/local/lib/python3.12/site-packages/:$PYTHONPATH" >>~/.bashrc
 # shellcheck disable=SC1091
 source "${HOME}/.bashrc"
-
 
 echo "** Install opencv ${version} successfully"
 echo "** Bye :)"
