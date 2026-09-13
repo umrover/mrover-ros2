378be0e684c3a6a5258b214183cd3422cefde277
kevinjin0420 <kevinjin0420@gmail.com>
2026-09-12 23:37:21 -0400

    cleanup

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 build_profile=RelWithDebInfo
 
 if [[ "$#" -ne "0" ]]; then
-    if [[ "$#" -eq "1" && ( "$1" == "Release" || "$1" == "RelWithDebInfo" || "$1" == "Debug" ) ]]; then
+    if [[ "$#" -eq "1" && ("$1" == "Release" || "$1" == "RelWithDebInfo" || "$1" == "Debug") ]]; then
         build_profile=$1
     else
         echo "Usage ./build.sh [Release|RelWithDebInfo|Debug]"
@@ -16,7 +16,7 @@ fi
 echo "Using build profile: $build_profile"
 
 if [ -n "${PIXI_PROJECT_ROOT:-}" ]; then
-	# portable environment
+    # portable environment
     os_cmake_args=()
     if [[ "$(uname)" == "Darwin" ]]; then
         macos_sysroot=$(xcrun --sdk macosx --show-sdk-path)
@@ -25,33 +25,32 @@ if [ -n "${PIXI_PROJECT_ROOT:-}" ]; then
 
     COLCON_EXTENSION_BLOCKLIST=colcon_core.event_handler.desktop_notification colcon build \
         --cmake-args -G Ninja -W no-dev \
-            -DCMAKE_BUILD_TYPE="${build_profile}" \
-            -DMROVER_PORTABLE=ON \
-            -DCMAKE_PREFIX_PATH="${CONDA_PREFIX}" \
-            "${os_cmake_args[@]}" \
+        -DCMAKE_BUILD_TYPE="${build_profile}" \
+        -DMROVER_PORTABLE=ON \
+        -DCMAKE_PREFIX_PATH="${CONDA_PREFIX}" \
+        "${os_cmake_args[@]}" \
         --symlink-install \
         --event-handlers console_direct+ \
         --packages-select mrover
 
-	# TODO(kevin), omitted $build_profile?
     ln -sf "$(pwd)/build/mrover/compile_commands.json" "$(pwd)/compile_commands.json"
 else
-	# native environment (ubuntu 24)
+    # native environment (ubuntu 24)
 
-	# Set C/C++ compilers
-	export CC=clang
-	export CXX=clang++
+    # Set C/C++ compilers
+    export CC=clang
+    export CXX=clang++
 
-	# Set CUDA compilers
-	export CUDAHOSTCXX=g++-9
-	export CUDACXX=/usr/local/cuda/bin/nvcc
+    # Set CUDA compilers
+    export CUDAHOSTCXX=g++-9
+    export CUDACXX=/usr/local/cuda/bin/nvcc
 
     # Build in the colcon workspace, not the package
     pushd ../..
 
     COLCON_EXTENSION_BLOCKLIST=colcon_core.event_handler.desktop_notification colcon build \
         --cmake-args -G Ninja -W no-dev \
-			-DCMAKE_BUILD_TYPE="$build_profile" \
+        -DCMAKE_BUILD_TYPE="$build_profile" \
         --symlink-install \
         --event-handlers console_direct+ \
         --build-base "build/$build_profile" \
